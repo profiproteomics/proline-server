c652734449250ab074ed972902870d433a3f95a8
Laurent Martin <laurent.martin2@cea.fr>
2013-02-11 17:22:41 +0000

    Add a trace log to see SQLite lock-file status.
    Revert MascotParser test logs to Debug.
    
    #7117

--- pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/RFImporterSQLiteTest.scala
+++ pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/RFImporterSQLiteTest.scala
@@ -1,38 +1,50 @@
 package fr.proline.core.service.msi
 
 import java.io.File
-import org.junit.{After,AfterClass, Assert, Test, Before,BeforeClass}
-import fr.proline.core.dal.SQLQueryHelper
+
+import org.junit.{ After, AfterClass, Assert, Before, BeforeClass, Test }
+
+import com.weiglewilczek.slf4s.Logging
+
 import fr.proline.core.om.model.msi.ResultSet
-import fr.proline.core.om.provider.msi.IResultSetProvider
-import fr.proline.core.om.provider.msi.impl.SQLResultSetProvider
-import fr.proline.core.om.storer.msi.impl.StorerContext
 import fr.proline.repository.DriverType
-import fr.proline.core.dal.ProlineEzDBC
-import fr.proline.context.IExecutionContext
+import fr.proline.util.StringUtils
 
 @AfterClass
 @BeforeClass
-object RFImporterSQLiteTest {
-  
+object RFImporterSQLiteTest extends Logging {
+
   // TODO: retrieve this value from the properties
   val MSI_SQLITE_MEMORY_LOCK_FILE = "msi_sqlite_shared_memory.lock"
   val UDS_SQLITE_MEMORY_LOCK_FILE = "uds_sqlite_shared_memory.lock"
   val PS_SQLITE_MEMORY_LOCK_FILE = "ps_sqlite_shared_memory.lock"
   val PDI_SQLITE_MEMORY_LOCK_FILE = "pdi_sqlite_shared_memory.lock"
-    
-  private def _deleteSQLiteLockFile(path:String) {
-    try {
-      // Very important: delete the SQLite locking file to release the memory
-      val sqliteLock = new java.io.File(path)
-      if( sqliteLock.exists() ) sqliteLock.delete()
-    } catch {
-      case npe: NullPointerException => {
-        System.err.println( path + " doesn't exists")
+
+  private def _deleteSQLiteLockFile(path: String) {
+
+    if (!StringUtils.isEmpty(path)) {
+
+      try {
+        val sqliteLock = new File(path)
+
+        if (sqliteLock.exists) {
+
+          if (sqliteLock.delete()) {
+            logger.debug("File [" + path + "] successfully deleted")
+          } else {
+            logger.warn("Unable to delete [" + path + ']')
+          }
+
+        }
+
+      } catch {
+        case ex: Exception => logger.error("Error deleteing [" + path + ']', ex)
       }
+
     }
+
   }
-  
+
   @AfterClass
   @BeforeClass
   def deleteSQLiteLockFiles() {
@@ -41,125 +53,123 @@ object RFImporterSQLiteTest {
     _deleteSQLiteLockFile(PS_SQLITE_MEMORY_LOCK_FILE)
     _deleteSQLiteLockFile(PDI_SQLITE_MEMORY_LOCK_FILE)
   }
-  
+
 }
 
 @Test
 class RFImporterSQLiteTest extends AbstractRFImporterTest_ {
-  
+
   val driverType = DriverType.SQLITE
-  
-  protected override def beforeAllTests() { }
-  protected override def afterAllTests() { }
+
+  protected override def beforeAllTests() {}
+  protected override def afterAllTests() {}
 
   @Before
-  @throws( classOf[Exception] )
+  @throws(classOf[Exception])
   override def setUp() = {
     RFImporterSQLiteTest.deleteSQLiteLockFiles()
     super.setUp()
-    _datFileName = "/dat_samples/STR_F122817_Mascot_v2.3.dat"    
-    udsDBTestCase.loadDataSet( "/fr/proline/module/parser/mascot/UDS_Simple_Dataset.xml" )
-    logger.info( "UDS db succesfully initialized" )
-    
+    _datFileName = "/dat_samples/STR_F122817_Mascot_v2.3.dat"
+    udsDBTestCase.loadDataSet("/fr/proline/module/parser/mascot/UDS_Simple_Dataset.xml")
+    logger.info("UDS db succesfully initialized")
+
     // Remove existing external DB connections
-   
+
     //val udsSqlHelper = new SQLQueryHelper( dbManagerForTest.asInstanceOf[DatabaseManager].getMsiDbConnector(1) ).ezDBC
     //sys.error( "" + udsSqlHelper.selectInt("select count(*) from scoring") )
-    
+
     ()
   }
-  
+
   @After
   override def tearDown() {
     super.tearDown()
   }
-  
- @Test
+
+  @Test
   def testRFIwithSQL() = {
     val (executionContext, rsProvider) = buildSQLContext()
-        Assert.assertNotNull( executionContext )
-    
-    logger.debug( " --- Get File " + _datFileName )
-    var datFile: File = new File( RFImporterSQLiteTest.this.getClass.getResource( _datFileName ).toURI )
+    Assert.assertNotNull(executionContext)
+
+    logger.debug(" --- Get File " + _datFileName)
+    var datFile: File = new File(RFImporterSQLiteTest.this.getClass.getResource(_datFileName).toURI)
 
     val propertiedBuilder = Map.newBuilder[String, Any]
-    propertiedBuilder += ( "ion.score.cutoff" -> 0.5 )
-    propertiedBuilder += ( "subset.threshold" -> 0.5 )
-    
+    propertiedBuilder += ("ion.score.cutoff" -> 0.5)
+    propertiedBuilder += ("subset.threshold" -> 0.5)
+
     val importer = new ResultFileImporterSQLStorer(
       executionContext,
       resultIdentFile = datFile,
       fileType = "MascotMSParser",
       instrumentConfigId = 1,
-      peaklistSoftwareId = 1,// TODO : provide the right value
-      importerProperties = Map.empty,      
-      acDecoyRegex = None
-    )
+      peaklistSoftwareId = 1, // TODO : provide the right value
+      importerProperties = Map.empty,
+      acDecoyRegex = None)
 
-    logger.debug( " --- run service " )
+    logger.debug(" --- run service ")
     val result = importer.runService()
     val id = importer.getTargetResultSetId
-    logger.debug( " --- done " + result + " save with resultID " + id )
+    logger.debug(" --- done " + result + " save with resultID " + id)
 
-    Assert.assertTrue( result )
-    Assert.assertNotNull( id )
-    Assert.assertTrue( id > 0 )
+    Assert.assertTrue(result)
+    Assert.assertNotNull(id)
+    Assert.assertTrue(id > 0)
 
-    val rsBackOp = rsProvider.getResultSet( id )
-    Assert.assertTrue( rsBackOp.isDefined )
+    val rsBackOp = rsProvider.getResultSet(id)
+    Assert.assertTrue(rsBackOp.isDefined)
     val rsBack: ResultSet = rsBackOp.get
-    Assert.assertNotNull( rsBack )
-    
+    Assert.assertNotNull(rsBack)
+
     //msiEzDBC.connection.close()
     //psEzDBC.connection.close()
 
     // Other verifs....
-    
+
     executionContext.closeAll()
   }
-  
+
   @Test
   def runRFIwithJPA() = {
     val (executionContext, rsProvider) = buildJPAContext()
-   
-    Assert.assertNotNull( executionContext )
-    
-    logger.debug( " --- Get File " + _datFileName )
-    var datFile: File = new File( RFImporterSQLiteTest.this.getClass.getResource( _datFileName ).toURI )
+
+    Assert.assertNotNull(executionContext)
+
+    logger.debug(" --- Get File " + _datFileName)
+    var datFile: File = new File(RFImporterSQLiteTest.this.getClass.getResource(_datFileName).toURI)
 
     val propertiedBuilder = Map.newBuilder[String, Any]
-    propertiedBuilder += ( "ion.score.cutoff" -> 0.5 )
-    propertiedBuilder += ( "subset.threshold" -> 0.5 )
-    
+    propertiedBuilder += ("ion.score.cutoff" -> 0.5)
+    propertiedBuilder += ("subset.threshold" -> 0.5)
+
     val importer = new ResultFileImporterJPAStorer(
       executionContext,
       resultIdentFile = datFile,
       fileType = "MascotMSParser",
       instrumentConfigId = 1,
-      peaklistSoftwareId = 1,// TODO : provide the right value
-      importerProperties = Map.empty,      
-      acDecoyRegex = None
-    )
+      peaklistSoftwareId = 1, // TODO : provide the right value
+      importerProperties = Map.empty,
+      acDecoyRegex = None)
 
-    logger.debug( " --- run service " )
+    logger.debug(" --- run service ")
     val result = importer.runService()
     val id = importer.getTargetResultSetId
-    logger.debug( " --- done " + result + " save with resultID " + id )
+    logger.debug(" --- done " + result + " save with resultID " + id)
 
-    Assert.assertTrue( result )
-    Assert.assertNotNull( id )
-    Assert.assertTrue( id > 0 )
+    Assert.assertTrue(result)
+    Assert.assertNotNull(id)
+    Assert.assertTrue(id > 0)
 
-    val rsBackOp = rsProvider.getResultSet( id )
-    Assert.assertTrue( rsBackOp.isDefined )
+    val rsBackOp = rsProvider.getResultSet(id)
+    Assert.assertTrue(rsBackOp.isDefined)
     val rsBack: ResultSet = rsBackOp.get
-    Assert.assertNotNull( rsBack )
-    
+    Assert.assertNotNull(rsBack)
+
     //msiEzDBC.connection.close()
     //psEzDBC.connection.close()
 
     // Other verifs....
-    
+
     executionContext.closeAll()
   }
 
