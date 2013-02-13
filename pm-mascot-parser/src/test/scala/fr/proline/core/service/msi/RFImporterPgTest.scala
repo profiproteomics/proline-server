2c71ff71c6fa03725bfe88a791cc051042b86e0d
Laurent Martin <laurent.martin2@cea.fr>
2013-02-13 16:18:45 +0000

    Add a 'Manual' JUnit test to check ResultSet import into PostgreSQL Database. (Test methods are @Ignore by default)
    Congiguration is in db_settings/postgresql/ resources directory.
    
    WARNING : There is a conflict with Peptides loaded by Unimod_Dataset.xml file => the sequence of Peptide entity table must be updated if Peptides are loaded.

---
+++ pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/RFImporterPgTest.scala
@@ -0,0 +1,123 @@
+package fr.proline.core.service.msi
+
+import fr.proline.repository.DriverType
+import org.junit.Before
+import org.junit.After
+import org.junit.Assert._
+import org.junit.Test
+import java.io.File
+import fr.proline.core.om.model.msi.ResultSet
+import org.junit.Ignore
+
+/* Manual PostgreSQL Test */
+class RFImporterPgTest extends AbstractRFImporterTest_ {
+
+  val driverType = DriverType.POSTGRESQL
+
+  @Before
+  @throws(classOf[Exception])
+  override def setUp() = {
+
+    super.setUp()
+
+    _datFileName = "/dat_samples/STR_F122817_Mascot_v2.3.dat"
+    udsDBTestCase.loadDataSet("/fr/proline/module/parser/mascot/UDS_Simple_Dataset.xml")
+    logger.info("UDS db succesfully initialized")
+  }
+
+  @After
+  override def tearDown() {
+    super.tearDown()
+  }
+
+  @Test
+  def testRFIwithSQL() = {
+    val (executionContext, rsProvider) = buildSQLContext
+
+    assertNotNull(executionContext)
+
+    try {
+      logger.debug(" --- Get File " + _datFileName)
+      var datFile: File = new File(getClass.getResource(_datFileName).toURI)
+
+      val propertiedBuilder = Map.newBuilder[String, Any]
+      propertiedBuilder += ("ion.score.cutoff" -> 0.5)
+      propertiedBuilder += ("subset.threshold" -> 0.5)
+
+      val importer: ResultFileImporterSQLStorer = new ResultFileImporterSQLStorer(
+        executionContext,
+        resultIdentFile = datFile,
+        fileType = "MascotMSParser",
+        instrumentConfigId = 1,
+        peaklistSoftwareId = 1, // TODO : provide the right value
+        importerProperties = propertiedBuilder.result,
+        acDecoyRegex = None)
+
+      logger.debug(" --- run service ")
+      val result = importer.runService()
+      val id = importer.getTargetResultSetId
+      logger.debug(" --- done " + result + " save with resultID " + id)
+
+      assertTrue(result)
+
+      assertTrue(id > 0)
+
+      val rsBackOp = rsProvider.getResultSet(id)
+      assertTrue(rsBackOp.isDefined)
+      val rsBack: ResultSet = rsBackOp.get
+      assertNotNull(rsBack)
+
+      // Other verifs....
+
+    } finally {
+      executionContext.closeAll()
+    }
+
+  }
+
+  @Ignore
+  def runRFIwithJPA() = {
+    val (executionContext, rsProvider) = buildJPAContext
+
+    assertNotNull(executionContext)
+
+    try {
+      logger.debug(" --- Get File " + _datFileName)
+      var datFile: File = new File(getClass.getResource(_datFileName).toURI)
+
+      val propertiedBuilder = Map.newBuilder[String, Any]
+      propertiedBuilder += ("ion.score.cutoff" -> 0.5)
+      propertiedBuilder += ("subset.threshold" -> 0.5)
+
+      val importer: ResultFileImporterJPAStorer = new ResultFileImporterJPAStorer(
+        executionContext,
+        resultIdentFile = datFile,
+        fileType = "MascotMSParser",
+        instrumentConfigId = 1,
+        peaklistSoftwareId = 1, // TODO : provide the right value
+        importerProperties = propertiedBuilder.result,
+        acDecoyRegex = None)
+
+      logger.debug(" --- run service ")
+      val result = importer.runService()
+      val id = importer.getTargetResultSetId
+      logger.debug(" --- done " + result + " save with resultID " + id)
+
+      assertTrue(result)
+
+      assertTrue(id > 0)
+
+      val rsBackOp = rsProvider.getResultSet(id)
+      assertTrue(rsBackOp.isDefined)
+      val rsBack: ResultSet = rsBackOp.get
+      assertNotNull(rsBack)
+
+      // Other verifs....
+
+    } finally {
+      executionContext.closeAll()
+    }
+
+  }
+
+}
