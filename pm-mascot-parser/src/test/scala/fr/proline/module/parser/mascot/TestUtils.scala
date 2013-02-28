9a1ab178477435c6092513c545cf8436c6bb2ebc
Laurent Martin <laurent.martin2@cea.fr>
2013-02-28 13:13:32 +0000

    Sort PeptideMatch by (sequence & score) to simplify diffs.
    refs #7177

--- pm-mascot-parser/src/test/scala/fr/proline/module/parser/mascot/TestUtils.scala
+++ pm-mascot-parser/src/test/scala/fr/proline/module/parser/mascot/TestUtils.scala
@@ -22,6 +22,26 @@ object TestUtils extends Logging {
   def savePeptideMatches(peptideMatches: Array[PeptideMatch]) = {
     val now = new Date()
 
+    def sorter(left: PeptideMatch, right: PeptideMatch): Boolean = {
+      var result: Boolean = false
+
+      val compare = left.peptide.sequence.compareTo(right.peptide.sequence)
+
+      if (compare < 0) {
+        result = true
+      } else if (compare == 0) {
+
+        if (left.score > right.score) { // Descending score
+          result = true
+        }
+
+      }
+
+      result
+    }
+
+    val sortedPeptideMatches = peptideMatches.sortWith(sorter)
+
     val osType = OSInfo.getOSType
 
     val outputFileName = String.format("peptide-matches_%s_%tY%<tm%<td%<tH%<tM%<tS_%<tL.txt", osType.name, now)
@@ -29,7 +49,7 @@ object TestUtils extends Logging {
     val pw = new PrintWriter(outputFileName, LATIN_1)
 
     try {
-      peptideMatches.foreach(pepMatch => {
+      sortedPeptideMatches.foreach(pepMatch => {
         val line = new StringBuilder(LINE_LENGTH)
         line.append(pepMatch.peptide.sequence).append(SEPARATOR)
         line.append(pepMatch.peptide.ptmString).append(SEPARATOR)
