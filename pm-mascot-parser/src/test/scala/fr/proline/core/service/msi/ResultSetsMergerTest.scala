24d9c149571412703cf4f4ea537a47ba08675425
Laurent Martin <laurent.martin2@cea.fr>
2013-05-02 09:48:04 +0000

    Force reloading of Decoy ResultSets (OM entities) before calling Merger service.
    refs #7547

--- pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/ResultSetsMergerTest.scala
+++ pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/ResultSetsMergerTest.scala
@@ -2,14 +2,17 @@ package fr.proline.core.service.msi
 
 import java.io.File
 
-import org.junit.{After, Assert}
-import org.junit.{Before, Ignore, Test}
+import scala.collection.mutable.ListBuffer
+
+import org.junit.{ After, Assert }
+import org.junit.{ Before, Ignore, Test }
 import org.junit.Assert._
 
 import com.weiglewilczek.slf4s.Logging
 
 import fr.proline.context.IExecutionContext
 import fr.proline.core.dal.ContextFactory
+import fr.proline.core.om.model.msi.ResultSet
 import fr.proline.core.om.provider.msi.IResultSetProvider
 import fr.proline.core.om.provider.msi.impl.ORMResultSetProvider
 import fr.proline.repository.DriverType
@@ -20,11 +23,6 @@ class ResultSetsMergerTest extends AbstractRFImporterTest_ with Logging {
   // For manual postgres test !! If use, should comment all loadDataSet from setUp and AbstractRFImporterTest_.setUp
   //   val driverType = DriverType.POSTGRESQL 
 
-  var executionContext: IExecutionContext = null
-  var rs1IDWork: Int = 0
-  var rs2IDWork: Int = 0
-  var rsProvider: IResultSetProvider = null
-
   @Before
   @throws(classOf[Exception])
   override def setUp() = {
@@ -32,15 +30,133 @@ class ResultSetsMergerTest extends AbstractRFImporterTest_ with Logging {
 
     udsDBTestCase.loadDataSet("/fr/proline/module/parser/mascot/UDS_Simple_Dataset.xml")
     logger.info("UDS db succesfully initialized")
+  }
 
-    val (execContext, rsProv) = buildSQLContext() //JPAContext() //SQLContext()    
-    executionContext = execContext
-    rsProvider = rsProv
+  @After
+  override def tearDown() {
+    super.tearDown()
+  }
+
+  @Test
+  def testMergeOneRS() {
+    val (sqlExecutionContext, rzProvider) = buildSQLContext()
+
+    var localJPAExecutionContext: IExecutionContext = null
+
+    try {
+      val rs1Id = importDatFile(sqlExecutionContext, "/dat_samples/STR_F136482_CTD.dat", """sp\|REV_\S+""")
+
+      val rsIds = Seq(rs1Id, rs1Id)
+
+      val rsMerger = new ResultSetMerger(sqlExecutionContext, loadResultSetsWithDecoy(rzProvider, rsIds))
+
+      val result = rsMerger.runService
+      Assert.assertTrue("ResultSet merger result", result)
+      logger.info("End Run ResultSetMerger Service, merge same RS twice, in Test ")
+
+      val tRSM = rsMerger.mergedResultSet
+      assertNotNull(tRSM)
+
+      val mergedDecoyRS = tRSM.decoyResultSet
+      assertTrue("Merged DECOY ResultSet is present", (mergedDecoyRS != null) && mergedDecoyRS.isDefined)
+
+      /* Try to reload merged ResultSet with JPA */
+      val mergedRSId = tRSM.id
+
+      localJPAExecutionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true)
+
+      val rsProvider = new ORMResultSetProvider(localJPAExecutionContext.getMSIDbConnectionContext, localJPAExecutionContext.getPSDbConnectionContext, localJPAExecutionContext.getPDIDbConnectionContext)
+
+      val optionalMergedRS = rsProvider.getResultSet(mergedRSId)
+      assertTrue("Reloaded Merged ResultSet", (optionalMergedRS != null) && optionalMergedRS.isDefined)
+
+      val optionalMergedDecoyRS = optionalMergedRS.get.decoyResultSet
+      assertTrue("Reloaded Merged DECOY ResultSet", (optionalMergedDecoyRS != null) && optionalMergedDecoyRS.isDefined)
+    } finally {
+
+      if (localJPAExecutionContext != null) {
+        try {
+          localJPAExecutionContext.closeAll()
+        } catch {
+          case exClose: Exception => logger.error("Error closing local JPA ExecutionContext", exClose)
+        }
+      }
+
+      if (sqlExecutionContext != null) {
+        try {
+          sqlExecutionContext.closeAll()
+        } catch {
+          case exClose: Exception => logger.error("Error closing JPA ExecutionContext", exClose)
+        }
+      }
+
+    }
 
   }
 
-  private def importDatFile(datFileClassPath: String, decoyRegExp: String): Int = {
+  @Test
+  def testMergeTwoRS() {
+    val (sqlExecutionContext, rzProvider) = buildSQLContext()
+
+    var localJPAExecutionContext: IExecutionContext = null
+
+    try {
+      val rs1Id = importDatFile(sqlExecutionContext, "/dat_samples/STR_F136482_CTD.dat", """sp\|REV_\S+""")
+      val rs2Id = importDatFile(sqlExecutionContext, "/dat_samples/STR_F122817_Mascot_v2.3.dat", """sp\|REV_\S+""")
+
+      val rsIds = Seq(rs1Id, rs2Id)
+
+      val rsMerger = new ResultSetMerger(sqlExecutionContext, loadResultSetsWithDecoy(rzProvider, rsIds))
+
+      val result = rsMerger.runService
+      assertTrue("ResultSet merger result", result)
+      logger.info("End Run ResultSetMerger Service, merge two different RS twice, in Test ")
+
+      val tRSM = rsMerger.mergedResultSet
+      assertNotNull(tRSM)
+
+      val mergedDecoyRS = tRSM.decoyResultSet
+      assertTrue("Merged DECOY ResultSet is present", (mergedDecoyRS != null) && mergedDecoyRS.isDefined)
+
+      /* Try to reload merged ResultSet with JPA */
+      val mergedRSId = tRSM.id
+
+      localJPAExecutionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true)
+
+      val rsProvider = new ORMResultSetProvider(localJPAExecutionContext.getMSIDbConnectionContext, localJPAExecutionContext.getPSDbConnectionContext, localJPAExecutionContext.getPDIDbConnectionContext)
+
+      val optionalMergedRS = rsProvider.getResultSet(mergedRSId)
+      assertTrue("Reloaded Merged ResultSet", (optionalMergedRS != null) && optionalMergedRS.isDefined)
+
+      val optionalMergedDecoyRS = optionalMergedRS.get.decoyResultSet
+      assertTrue("Reloaded Merged DECOY ResultSet", (optionalMergedDecoyRS != null) && optionalMergedDecoyRS.isDefined)
+    } finally {
+
+      if (localJPAExecutionContext != null) {
+        try {
+          localJPAExecutionContext.closeAll()
+        } catch {
+          case exClose: Exception => logger.error("Error closing local JPA ExecutionContext", exClose)
+        }
+      }
+
+      if (sqlExecutionContext != null) {
+        try {
+          sqlExecutionContext.closeAll()
+        } catch {
+          case exClose: Exception => logger.error("Error closing SQL ExecutionContext", exClose)
+        }
+      }
+
+    }
+
+  }
+
+  /* Private methods */
+  private def importDatFile(localExecutionContext: IExecutionContext, datFileClassPath: String, decoyRegExp: String): Int = {
     logger.debug(" --- Load Mascot File " + datFileClassPath)
+
+    val beforePeptideMatch = getPeptideMatchCount
 
     var datFile: File = new File(getClass.getResource(datFileClassPath).toURI)
 
@@ -49,7 +165,7 @@ class ResultSetsMergerTest extends AbstractRFImporterTest_ with Logging {
     propertiedBuilder += ("subset.threshold" -> 1.0)
 
     val importer: ResultFileImporterSQLStorer = new ResultFileImporterSQLStorer(
-      executionContext,
+      localExecutionContext,
       resultIdentFile = datFile,
       fileType = "MascotMSParser",
       instrumentConfigId = 1,
@@ -60,89 +176,83 @@ class ResultSetsMergerTest extends AbstractRFImporterTest_ with Logging {
     val result = importer.runService()
     val rsID = importer.getTargetResultSetId
 
-    logger.debug(" ResultFile  loaded (" + result + ") from " + datFile.getAbsolutePath() + " with target resultID " + rsID)
+    val localJPAContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true)
+
+    val afterPeptideMatch = getPeptideMatchCount
+
+    logger.info("TOTAL PeptideMatches created : " + (afterPeptideMatch - beforePeptideMatch))
+
+    logger.debug("ResultFile result (" + result + ") from " + datFile.getAbsolutePath() + " with target resultID " + rsID)
+
     rsID
   }
 
-  @After
-  override def tearDown() {
-    if (executionContext != null)
-      executionContext.closeAll()
-    super.tearDown()
-  }
+  private def getPeptideMatchCount(): Long = {
+    var result: Long = 0
 
-  @Test
-  def testMergeOneRS() = {
-
-    rs1IDWork = importDatFile("/dat_samples/STR_F136482_CTD.dat", """sp\|REV_\S+""")
-
-    val rsIDs = Seq(rs1IDWork, rs1IDWork)
-
-    val rsMerger = ResultSetMerger(
-      execContext = executionContext,
-      resultSetIds = rsIDs)
-
-    val result = rsMerger.runService
-    Assert.assertTrue("ResultSet merger result", result)
-    logger.info(" End Run ResultSetMerger Service, merge same RS twice, in Test ")
-
-    val tRSM = rsMerger.mergedResultSet
-
-    assertNotNull(tRSM)
-
-    val mergedDecoyRS = tRSM.decoyResultSet
-
-    assertTrue("Merged DECOY ResultSet is present", (mergedDecoyRS != null) && mergedDecoyRS.isDefined)
-  }
-
-  @Test
-  def testMergeTwoRS() = {
-    rs1IDWork = importDatFile("/dat_samples/STR_F136482_CTD.dat", """sp\|REV_\S+""")
-    rs2IDWork = importDatFile("/dat_samples/STR_F122817_Mascot_v2.3.dat", """sp\|REV_\S+""")
-    val rsIDs = Seq(rs1IDWork, rs2IDWork)
-
-    val rsMerger = ResultSetMerger(
-      execContext = executionContext,
-      resultSetIds = rsIDs)
-
-    val result = rsMerger.runService
-    assertTrue("ResultSet merger result", result)
-    logger.info(" End Run ResultSetMerger Service, merge two different RS twice, in Test ")
-
-    val tRSM = rsMerger.mergedResultSet
-
-    assertNotNull(tRSM)
-
-    val mergedDecoyRS = tRSM.decoyResultSet
-
-    assertTrue("Merged DECOY ResultSet is present", (mergedDecoyRS != null) && mergedDecoyRS.isDefined)
-
-    /* Try to reload merged ResultSet with JPA */
-    val mergedRSId = tRSM.id
-
-    val localJPAContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true)
+    val msiEM = dsConnectorFactoryForTest.getMsiDbConnector(1).getEntityManagerFactory.createEntityManager()
 
     try {
-      val rsProvider = new ORMResultSetProvider(localJPAContext.getMSIDbConnectionContext, localJPAContext.getPSDbConnectionContext, localJPAContext.getPDIDbConnectionContext)
+      val query = msiEM.createQuery("select count(distinct pm) from fr.proline.core.orm.msi.PeptideMatch pm")
 
-      val optionalMergedRS = rsProvider.getResultSet(mergedRSId)
-      assertTrue("Reloaded Merged ResultSet", (optionalMergedRS != null) && optionalMergedRS.isDefined)
+      val obj = query.getSingleResult
 
-      val optionalMergedDecoyRS = optionalMergedRS.get.decoyResultSet
-      assertTrue("Reloaded Merged DECOY ResultSet", (optionalMergedDecoyRS != null) && optionalMergedDecoyRS.isDefined)
+      if (obj.isInstanceOf[java.lang.Long]) {
+        result = obj.asInstanceOf[java.lang.Long].longValue
+      }
 
     } finally {
 
-      if (localJPAContext != null) {
+      if (msiEM != null) {
         try {
-          localJPAContext.closeAll()
+          msiEM.close()
         } catch {
-          case exClose: Exception => logger.error("Error closing local JPA ExecutionContext", exClose)
+          case exClose: Exception => logger.error("Error closing MSI EntityManager", exClose)
         }
       }
 
     }
 
+    result
+  }
+
+  private def loadResultSetsWithDecoy(rsProvider: IResultSetProvider, rsIds: Seq[Int]): Seq[ResultSet] = {
+    val loadedRS = ListBuffer.empty[ResultSet]
+
+    for (rsId <- rsIds) {
+      val optionalTargetRS = rsProvider.getResultSet(rsId)
+
+      if ((optionalTargetRS == null) || optionalTargetRS.isEmpty) {
+        logger.warn("TARGET ResultSet #" + rsId + " NOT found in MSI Db")
+      } else {
+        val targetRS = optionalTargetRS.get
+
+        val optionalDecoyRS = targetRS.decoyResultSet
+        if ((optionalDecoyRS == null) || optionalDecoyRS.isEmpty) {
+          val decoyRSId = targetRS.getDecoyResultSetId
+
+          if (decoyRSId != 0) {
+            /* Force loading of DECOY ResultSet */
+            val optionalLoadedDecoyRS = rsProvider.getResultSet(decoyRSId)
+
+            if ((optionalLoadedDecoyRS == null) || optionalLoadedDecoyRS.isEmpty) {
+              logger.warn("DECOY ResultSet #" + decoyRSId + " NOT found in MSI Db")
+            } else {
+              targetRS.decoyResultSet = Some(optionalLoadedDecoyRS.get) // Attach Decoy ResultSet entity
+            }
+
+          } else {
+            logger.warn("ResultSet #" + rsId + "has no associated DECOY ResultSet")
+          }
+
+        }
+
+        loadedRS += targetRS
+      }
+
+    }
+
+    loadedRS.result
   }
 
 }
