3b3a88724c2cbaf7c0054aacbc012b1b3f31a897
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-11-05 12:17:10 +0000

    Add a JUnit test for the export of merged RSMs Note: the content of the exported file is not checked, we only know that no exception is thrown during the RSM export
    #8487

---
+++ pm-dataset-exporter/src/test/scala/fr/proline/module/exporter/msi/ViewSetExporterTest.scala
@@ -0,0 +1,86 @@
+package fr.proline.module.exporter.msi
+
+import org.junit._
+import org.apache.commons.io.FileUtils
+import java.nio.file.Files
+import com.weiglewilczek.slf4s.Logging
+
+import fr.proline.context.IExecutionContext
+import fr.proline.core.dal.ContextFactory
+import fr.proline.core.om.provider.msi.impl.SQLResultSummaryProvider
+import fr.proline.core.om.utils.AbstractMultipleDBTestCase
+import fr.proline.module.exporter.msi.template.IRMaLikeViewSetTemplateAsXLSX
+import fr.proline.module.exporter.msi.view.BuildResultSummaryViewSet
+import fr.proline.module.exporter.commons.ViewSetExporter
+import fr.proline.repository.DriverType
+
+/**
+ * @author David Bouyssie
+ *
+ */
+class ViewSetExporterTest  extends AbstractMultipleDBTestCase with Logging {
+
+  // Define some vars
+  val driverType = DriverType.H2
+  val fileName = "STR_F063442_F122817_MergedRSMs"
+  val targetRSMId: Long = 33
+
+  var executionContext: IExecutionContext = null
+
+  @Before
+  @throws(classOf[Exception])
+  def setUp() = {
+
+    logger.info("Initializing DBs")
+    super.initDBsDBManagement(driverType)
+
+    // Load Data
+    pdiDBTestCase.loadDataSet("/dbunit/datasets/pdi/Proteins_Dataset.xml")
+    psDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/ps-db.xml")
+    msiDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/msi-db.xml")
+    udsDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/uds-db.xml")
+
+    logger.info("PDI, PS, MSI and UDS dbs successfully initialized !")
+
+    executionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true) // Full JPA
+  }
+
+  @After
+  override def tearDown() {
+    if (executionContext != null) executionContext.closeAll()
+    super.tearDown()
+  }
+  
+  @Test
+  def testExportViewSetToDir() {
+
+    val rsmProvider = new SQLResultSummaryProvider(executionContext.getMSIDbConnectionContext, executionContext.getPSDbConnectionContext, executionContext.getUDSDbConnectionContext)
+    val rsm = rsmProvider.getResultSummary(targetRSMId, true).get
+    val viewSet = BuildResultSummaryViewSet(rsm,fileName,IRMaLikeViewSetTemplateAsXLSX )
+    
+    // Create TEMP dir
+    val exportPath = Files.createTempDirectory(null)
+    val exportLoc = exportPath.toFile
+    
+    var exception = Option.empty[Exception]
+    try {
+      
+      // Export the RSM
+      ViewSetExporter.exportViewSetToDirectory(viewSet, exportLoc )
+      
+      // TODO: compare the content of the exported file with an expected content
+      
+    } catch {
+      case e: Exception => exception = Some(e)
+    }
+    finally {
+      // Remove temp dir
+      FileUtils.deleteDirectory(exportLoc)
+    }
+
+    // Throw exception if catched
+    exception.map( throw _ )
+
+  }
+
+}
