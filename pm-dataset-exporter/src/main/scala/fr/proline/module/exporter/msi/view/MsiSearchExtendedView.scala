5192f72f9654410907ecf07312578cadd86ea88c
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-11-06 14:42:28 +0000

    Add missing parameters in the MSI Search view
    #8297

--- pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/msi/view/MsiSearchExtendedView.scala
+++ pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/msi/view/MsiSearchExtendedView.scala
@@ -4,20 +4,30 @@ import fr.proline.module.exporter.api.view._
 import fr.proline.core.om.model.msi._
 
 object MsiSearchExtendedViewFields extends IViewFieldEnumeration {
+  
   val SEARCH_TITLE = Field("search_title")
   val SEARCH_DATE = Field("search_date")
+  val RAW_FILE_NAME = Field("raw_file_name")
+  val PEAKLIST_FILE_PATH = Field("peaklist_file_path")
   val RESULT_FILE_NAME = Field("result_file_name")
   val RESULT_FILE_DIRECTORY = Field("result_file_directory")
   val JOB_NUMBER = Field("job_number")
   val USER_NAME = Field("user_name")
   val USER_EMAIL = Field("user_email")
+  
   val QUERIES_COUNT = Field("queries_count")
   val SUBMITTED_QUERIES_COUNT = Field("submitted_queries_count")
   val SEARCHED_SEQUENCES_COUNT = Field("searched_sequences_count")
   val SOFTWARE_NAME = Field("software_name")
   val SOFTWARE_VERSION = Field("software_version")
+  
+  val MAIN_DATABASE_NAME = Field("main_database_name")
+  val MAIN_DATABASE_RELEASE = Field("main_database_release")
   val TAXONOMY = Field("taxonomy")
+  val ENZYMES = Field("enzymes")
   val MAX_MISSED_CLEAVAGES = Field("max_missed_cleavages")
+  val FIXED_PTMS = Field("fixed_ptms")
+  val VARIABLE_PTMS = Field("variable_ptms")  
   val PEPTIDE_CHARGE_STATES = Field("peptide_charge_states")
   val PEPTIDE_MASS_ERROR_TOLERANCE = Field("peptide_mass_error_tolerance")
   val PEPTIDE_MASS_ERROR_TOLERANCE_UNIT = Field("peptide_mass_error_tolerance_unit")
@@ -60,12 +70,15 @@ class MsiSearchExtendedView( val rsm: ResultSummary ) extends IDatasetView {
     
     val myBuildingContext = buildingContext.asInstanceOf[MyBuildingContext]
     val msiSearch = myBuildingContext.msiSearch
+    val pkl = msiSearch.peakList
     val searchSettings = msiSearch.searchSettings
 
     // TODO: add missing fields (from other tables than msi_search and search_settings)
     Map(
       fields.SEARCH_TITLE -> msiSearch.title,
       fields.SEARCH_DATE -> msiSearch.date,
+      fields.RAW_FILE_NAME -> Option(pkl.rawFileName).getOrElse(""),
+      fields.PEAKLIST_FILE_PATH -> pkl.path,
       fields.RESULT_FILE_NAME -> msiSearch.resultFileName,
       fields.RESULT_FILE_DIRECTORY -> msiSearch.resultFileDirectory,
       fields.JOB_NUMBER -> msiSearch.jobNumber,
@@ -76,8 +89,13 @@ class MsiSearchExtendedView( val rsm: ResultSummary ) extends IDatasetView {
       fields.SEARCHED_SEQUENCES_COUNT -> msiSearch.searchedSequencesCount,
       fields.SOFTWARE_NAME -> searchSettings.softwareName,
       fields.SOFTWARE_VERSION -> searchSettings.softwareVersion,
+      fields.MAIN_DATABASE_NAME -> searchSettings.seqDatabases(0).name,
+      fields.MAIN_DATABASE_RELEASE -> searchSettings.seqDatabases(0).releaseDate,
       fields.TAXONOMY -> searchSettings.taxonomy,
+      fields.ENZYMES -> searchSettings.usedEnzymes.map(_.name).mkString(", "),
       fields.MAX_MISSED_CLEAVAGES -> searchSettings.maxMissedCleavages,
+      fields.FIXED_PTMS -> searchSettings.fixedPtmDefs.map( _.toReadableString ).mkString("; "),
+      fields.VARIABLE_PTMS -> searchSettings.variablePtmDefs.map( _.toReadableString ).mkString("; "),
       fields.PEPTIDE_CHARGE_STATES -> searchSettings.ms1ChargeStates,
       fields.PEPTIDE_MASS_ERROR_TOLERANCE -> searchSettings.ms1ErrorTol,
       fields.PEPTIDE_MASS_ERROR_TOLERANCE_UNIT -> searchSettings.ms1ErrorTolUnit,
@@ -89,8 +107,8 @@ class MsiSearchExtendedView( val rsm: ResultSummary ) extends IDatasetView {
   def onEachRecord( recordFormatter: Map[String,Any] => Unit ) {
     
     val rs = rsm.resultSet.get
-    for( msiSearch <- rs.msiSearch ) {      
-      this.formatRecord(MyBuildingContext(msiSearch), recordFormatter)      
+    for( msiSearch <- rs.msiSearch if msiSearch != null ) {
+      this.formatRecord(MyBuildingContext(msiSearch), recordFormatter)
     }
     
   }
