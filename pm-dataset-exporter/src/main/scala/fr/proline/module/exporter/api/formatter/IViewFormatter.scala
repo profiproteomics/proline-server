c42a70948d17a535143a85d0716ac064d8b3552b
Laurent Martin <laurent.martin2@cea.fr>
2013-11-26 10:36:26 +0000

    Use @NamedParamsRetriever.getOptString(name, null)@ to retrieve optional param String.
    Generate unique file name when exporting to tlv
    
    Replace some "/" by File.separatorChar when dealing with Plateform regular file.
    
    refs #8486

--- pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/api/formatter/IViewFormatter.scala
+++ pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/api/formatter/IViewFormatter.scala
@@ -5,41 +5,45 @@ import java.io.FileOutputStream
 import java.io.OutputStream
 import fr.proline.module.exporter.api.view.IDatasetView
 import fr.proline.module.exporter.api.template.IViewTemplate
+import java.util.UUID
 
 trait IViewFormatter {
-  
+
   val template: IViewTemplate
   val fileExtension: String = template.fileExtension
-  
-  def getViewLocation( viewDir: File, view: IDatasetView ): File = {
-    require( viewDir.isDirectory, "viewDir must be a directory" )
-    new File( viewDir.getAbsolutePath() + "/" + view.viewName + "." + fileExtension )
+
+  def getViewLocation(viewDir: File, view: IDatasetView): File = {
+    require(((viewDir != null) && viewDir.isDirectory), "viewDir must be a directory")
+
+    // Create a unique name for this view
+    val fileBaseName = view.viewName + '_' + UUID.randomUUID().toString + '.' + fileExtension
+    new File(viewDir, fileBaseName)
   }
-  def getViewSetLocation( viewDir: File, viewSetName: String ): File = {
-    new File( viewDir.getAbsolutePath() )
+  def getViewSetLocation(viewDir: File, viewSetName: String): File = {
+    new File(viewDir.getAbsolutePath())
   }
-  
-  def formatView( view: IDatasetView, os: OutputStream )
-  
-  def formatView( view: IDatasetView, location: File ): File = {
-    
-    if( location.isFile() || location.exists() == false ) {
-      _formatView( view, location )
+
+  def formatView(view: IDatasetView, os: OutputStream)
+
+  def formatView(view: IDatasetView, location: File): File = {
+
+    if (location.isFile() || location.exists() == false) {
+      _formatView(view, location)
       location
     } else {
-      val file = this.getViewLocation( location, view )
-      _formatView( view, file )
+      val file = this.getViewLocation(location, view)
+      _formatView(view, file)
       file
     }
 
   }
-  
-  protected def _formatView( view: IDatasetView, outputFile: File ) {
+
+  protected def _formatView(view: IDatasetView, outputFile: File) {
     val fop = new FileOutputStream(outputFile)
-    this.formatView( view, fop )
+    this.formatView(view, fop)
     fop.close()
   }
-  
+
   /*def formatView( view: IDatasetView, outputDir: File, fileName: String ) {
     require( outputDir.isDirectory == true, "outputDir must be a directory" )
     
@@ -48,5 +52,5 @@ trait IViewFormatter {
     this.formatView( view, fop )
     fop.close()
   }*/
-  
+
 }
--- pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/commons/formatter/SpreadsheetFormatter.scala
+++ pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/commons/formatter/SpreadsheetFormatter.scala
@@ -8,29 +8,31 @@ import fr.proline.module.exporter.api.formatter.IViewFormatter
 import fr.proline.module.exporter.commons.template.IWorksheetTemplate
 
 class SpreadsheetFormatter(
-  val template: IWorksheetTemplate
-) extends IViewFormatter {
-  
-  override def getViewSetLocation( viewDir: File, viewSetName: String ): File = {
-    new File( viewDir.getAbsolutePath() + "/" + viewSetName + "." + fileExtension )
+  val template: IWorksheetTemplate) extends IViewFormatter {
+
+  override def getViewSetLocation(viewDir: File, viewSetName: String): File = {
+    require(((viewDir != null) && viewDir.isDirectory), "viewDir must be a directory")
+
+    val fileBaseName = viewSetName + '.' + fileExtension
+    new File(viewDir, fileBaseName)
   }
-  
-  def formatView( view: IDatasetView, os: OutputStream ) {
+
+  def formatView(view: IDatasetView, os: OutputStream) {
     throw new Exception("not yet implemented")
   }
-  
+
   // TODO: use Apache POI or equivalent to export some views
-  override def formatView( view: IDatasetView, location: File ): File = {
-    require( location.isDirectory == false, "location must be a file" )
-    
+  override def formatView(view: IDatasetView, location: File): File = {
+    require(location.isDirectory == false, "location must be a file")
+
     // Create the workbook if it doesn't exist
-    if( location.exists() == false ) {
-      template.newWorkbook( location )
+    if (location.exists() == false) {
+      template.newWorkbook(location)
     }
-    
+
     // Append a new worksheet to the workbook for this view
-    template.newWorksheet( view, location )
-    
+    template.newWorksheet(view, location)
+
     location
   }
 
