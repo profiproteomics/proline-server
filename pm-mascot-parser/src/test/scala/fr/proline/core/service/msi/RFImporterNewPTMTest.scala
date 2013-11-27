a01a8d979e58cbe3c2ee353774fb37f6efff28bd
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-11-27 18:39:06 +0000

    Make some additional checks (number of inserted PTM specificities and evidences)
    #8765

--- pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/RFImporterNewPTMTest.scala
+++ pm-mascot-parser/src/test/scala/fr/proline/core/service/msi/RFImporterNewPTMTest.scala
@@ -1,13 +1,13 @@
 package fr.proline.core.service.msi
 
 import java.io.File
-
+import scala.collection.JavaConversions.collectionAsScalaIterable
 import org.junit.{Before, Ignore, Test}
 import org.junit.After
 import org.junit.Assert._
-
 import fr.proline.repository.DriverType
 import fr.proline.module.parser.mascot.MascotResultFileProvider
+import fr.proline.core.om.model.msi.IonTypes
 
 @Test
 class RFImporterNewPTMTest extends AbstractRFImporterTest_ {
@@ -47,9 +47,18 @@ class RFImporterNewPTMTest extends AbstractRFImporterTest_ {
       val mascotRFProvider = new MascotResultFileProvider()
       val ptmDefs = mascotRFProvider.getPtmDefinitions(datFile, Map.empty)
       
+      // Check the number of parsed PTMs
       assertEquals( 18, ptmDefs.length )
-      assertEquals( "H(3) C(2) N O", ptmDefs(0).ptmEvidences(0).composition)
-      assertEquals( "Carbamidomethyl", ptmDefs(0).names.shortName)
+      
+      // Retrieve the Hydroxylation PTM definition specific to the Methionine residue
+      val methPtmDef = ptmDefs.find( ptmDef => ptmDef.names.shortName == "Hydroxylation" && ptmDef.residue == 'M' ).get
+      /*for( ev <- methPtmDef.ptmEvidences ) {
+        println("type="+ev.ionType+" mono mass="+ev.monoMass + " comp="+ev.composition )
+      }*/
+      
+      // Check the composition and the number of PTM evidences of the retrieved PTM definition
+      assertEquals( "O", methPtmDef.precursorDelta.composition )
+      assertEquals( 2, methPtmDef.ptmEvidences.filter( _.ionType != IonTypes.Precursor ).length )
       
       logger.debug("--- Certifying result file")
       
@@ -68,7 +77,25 @@ class RFImporterNewPTMTest extends AbstractRFImporterTest_ {
       val ptmCountAfterCertif = psEM.createQuery("FROM fr.proline.core.orm.ps.Ptm").getResultList().size
       logger.debug(ptmCountAfterCertif+" PTMs found in PSdb after result file certification" )
       
+      // Check we have now an additional PTM in the database
       assertEquals( ptmCountBeforeCertif + 1, ptmCountAfterCertif )
+      
+      // Retrieve Hydroxylation PTM
+      val psPTM = psEM.createQuery(
+        "FROM fr.proline.core.orm.ps.Ptm WHERE short_name='Hydroxylation'",
+        classOf[fr.proline.core.orm.ps.Ptm]
+      ).getSingleResult()
+      
+      // Check we have inserted the right number of PTM specificities
+      val ptmSpecifs = psPTM.getSpecificities().toList
+      assertEquals( 11, ptmSpecifs.size )
+      
+      // Check we have inserted the right number of PTM evidences for the Methionine specificity
+      val methSpecif = ptmSpecifs.find( _.getResidue() == 'M' ).get
+      /*for( ev <- methSpecif.getEvidences().toList ) {
+        println("type="+ev.getType()+" mono mass="+ev.getMonoMass + " comp="+ev.getComposition )
+      }*/
+      assertEquals( 2, methSpecif.getEvidences().size )
       
       logger.debug("--- Importing result file")
       val importer = new ResultFileImporter(
