a67f4b61a38986c303518a81855d2b9bb89a9541
Christophe Bruley <christophe.bruley@cea.fr>
2014-06-04 09:14:58 +0000

    - An alternative implementation of FragmentIonTable using an OM Peptide as input - Implementation of a  Peptide-Spectrum Matcher algo

---
+++ pm-fragmentmatch-generator/src/main/scala/fr/proline/module/fragment_match/FragmentIonTableV2.scala
@@ -0,0 +1,278 @@
+package fr.proline.module.fragment_match
+
+import java.util.ArrayList
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import scala.collection.JavaConversions.asJavaList
+import fr.proline.core.om.model.msi.TheoreticalFragmentSeries
+import fr.proline.core.om.model.msi.PtmDefinition
+import com.typesafe.scalalogging.slf4j.Logging
+import fr.proline.core.om.model.msi.Peptide
+import fr.profi.chemistry.model.MolecularConstants
+import fr.profi.chemistry.model.AminoAcidTable
+import fr.profi.chemistry.model.HumanAminoAcidTable
+import fr.proline.core.om.model.msi.LocatedPtm
+import fr.profi.chemistry.model.AminoAcidResidue
+import fr.profi.chemistry.model.AtomTable
+import fr.profi.chemistry.model.BiomoleculeAtomTable
+import fr.proline.core.om.model.msi.PtmEvidence
+import fr.proline.core.om.model.msi.FragmentMatchType
+import fr.proline.core.om.model.msi.FragmentIonType
+import fr.proline.core.om.model.msi.Fragmentation
+import fr.proline.util.MathUtils
+
+
+case class Fragment(moz: Double, position: Int, neutralLoss: Double = 0.0, fragmentType: Option[FragmentMatchType.Value] = None, series: Option[String] = None, charge: Int = 1)
+
+object FragmentIonTableV2 {
+
+  def fragmentIonTableAsString(_table: Array[TheoreticalFragmentSeries]): String = {
+    var text = "Fragment ion table:\n"
+    _table.foreach(serie => {
+      text += "Serie " + serie.ionSeries + List.fill(serie.charge)("+").mkString + " ("
+      text += serie.masses.map("%1.4f".format(_)).mkString(";")
+      text += ")\n"
+    })
+    text
+  }
+
+  val aminoAcids = {
+    val hmBuilder = HashMap.newBuilder[Char, AminoAcidResidue]
+    val aa = AminoAcidTable(HumanAminoAcidTable.aminoAcids)
+    for (entity <- HumanAminoAcidTable.aminoAcids) hmBuilder += (entity.code1 -> entity)
+    hmBuilder += ('X' -> AminoAcidResidue(
+      code1 = 'X',
+      code3 = "Xaa",
+      name = "Unknown",
+      formula = "",
+      monoMass = MolecularConstants.AVERAGE_AA_MASS,
+      averageMass = MolecularConstants.AVERAGE_AA_MASS,
+      occurrence = 0.0f))
+    hmBuilder += ('B' -> AminoAcidResidue(
+      code1 = 'B',
+      code3 = "Asx",
+      name = "Asn or Asp",
+      formula = "",
+      monoMass = (aa.getAminoAcid("N").monoMass + aa.getAminoAcid("D").monoMass) / 2.0,
+      averageMass = (aa.getAminoAcid("N").averageMass + aa.getAminoAcid("D").averageMass) / 2.0,
+      occurrence = 0.0f))
+    hmBuilder += ('Z' -> AminoAcidResidue(
+      code1 = 'Z',
+      code3 = "Glx",
+      name = "Glu or Gln",
+      formula = "",
+      monoMass = (aa.getAminoAcid("E").monoMass + aa.getAminoAcid("Q").monoMass) / 2.0,
+      averageMass = (aa.getAminoAcid("E").averageMass + aa.getAminoAcid("Q").averageMass) / 2.0,
+      occurrence = 0.0f))
+
+    hmBuilder.result
+  }
+
+  val atoms = AtomTable(BiomoleculeAtomTable.atoms)
+  val H2O = 2 * atoms.getAtom("H").monoMass + atoms.getAtom("O").monoMass
+  val NH3 = 3 * atoms.getAtom("H").monoMass + atoms.getAtom("N").monoMass
+  val CO = atoms.getAtom("C").monoMass + atoms.getAtom("O").monoMass
+
+}
+
+class FragmentIonTableV2(peptide: Peptide,
+  currentFragmentIonTypes: FragmentIons,
+  sequence: Option[Array[Char]] = None,
+  ptmNeutralLosses: Option[Map[LocatedPtm, Double]] = None) extends Logging {
+  
+  val fragments: HashMap[String, ArrayBuffer[Fragment]] = {
+    val _table = new HashMap[String, ArrayBuffer[Fragment]]
+    val ptmByPosition = new HashMap[Int, LocatedPtm]
+    
+    if (peptide.ptms != null)
+      ptmByPosition ++= peptide.ptms.map { ptm => (ptm.seqPosition -> ptm) }
+
+    var mass = MolecularConstants.WATER_MONO_MASS
+    val nlResidues = Map("H2O" -> ArrayBuffer(0, 0), "NH3" -> ArrayBuffer(0, 0))
+    val aaSequence = if (sequence.isDefined) sequence.get else peptide.sequence.toCharArray()
+    for (aa <- aaSequence) {
+      mass += FragmentIonTableV2.aminoAcids(aa).monoMass
+      aa match {
+        case 'R' | 'K' | 'N' | 'Q' => nlResidues("NH3")(1) += 1
+        case 'S' | 'T' | 'E' | 'D' => nlResidues("H2O")(1) += 1
+        case _ =>
+      }
+    }
+   
+    var nterMass = 0.0
+    var cterPtmNLMasse = 0.0
+    
+    if (peptide.ptms != null) {
+      for (ptm <- peptide.ptms) {
+        val nlMass = {
+          if (ptmNeutralLosses.isDefined && ptmNeutralLosses.get.contains(ptm)) {
+            ptmNeutralLosses.get(ptm)
+          } else 0
+        }
+        mass = mass + ptm.monoMass - nlMass
+        cterPtmNLMasse += nlMass
+        if (ptm.isNTerm) nterMass += ptm.monoMass
+      }
+    }
+        
+        
+    var bFragmentMz = MolecularConstants.PROTON_MASS + nterMass
+    var position = 0
+    var nterPtmlNLMasse = 0.0
+    var reversePosition = aaSequence.length
+    
+    // initialize last reverse ions fragments to 0.0
+    if (currentFragmentIonTypes.contains("y")) {
+      addSeries(_table, "y", currentFragmentIonTypes.getCharge("y"), new Fragment(0.0, reversePosition, cterPtmNLMasse))
+      addSeries(_table, "y*", currentFragmentIonTypes.getCharge("y"), new Fragment(0.0, reversePosition, cterPtmNLMasse))
+      addSeries(_table, "y0", currentFragmentIonTypes.getCharge("y"), new Fragment(0.0, reversePosition, cterPtmNLMasse))
+    }
+    
+    if (currentFragmentIonTypes.contains("x"))  addSeries(_table, "x", currentFragmentIonTypes.getCharge("x"), new Fragment(0.0, reversePosition, cterPtmNLMasse))
+    if (currentFragmentIonTypes.contains("z"))  addSeries(_table, "z", currentFragmentIonTypes.getCharge("z"), new Fragment(0.0, reversePosition, cterPtmNLMasse))
+    
+    position += 1
+    reversePosition -= 1
+    
+    for (aa <- aaSequence.dropRight(1)) {
+      val residue = FragmentIonTableV2.aminoAcids(aa)
+      val (ptmMass, ptmNLMass)  =  { 
+        if (!ptmByPosition.contains(position)) 
+          (0.0, 0.0)
+        else {
+          val ptm = ptmByPosition.get(position).get
+          (ptm.monoMass, if (ptmNeutralLosses.isDefined && ptmNeutralLosses.isDefined && ptmNeutralLosses.get.contains(ptm)) ptmNeutralLosses.get(ptm) else 0.0)
+        }
+      }
+    
+      bFragmentMz = bFragmentMz + residue.monoMass + ptmMass - ptmNLMass
+      nterPtmlNLMasse += ptmNLMass
+      cterPtmNLMasse -= ptmNLMass
+      val yFragmentMz = mass - bFragmentMz + 2 * MolecularConstants.PROTON_MASS
+      aa match {
+        case 'R' | 'K' | 'N' | 'Q' =>
+          nlResidues("NH3")(0) += 1
+          nlResidues("NH3")(1) -= 1
+        case 'S' | 'T' | 'E' | 'D' =>
+          nlResidues("H2O")(0) += 1
+          nlResidues("H2O")(1) -= 1
+        case _ =>
+      }
+
+      if (currentFragmentIonTypes.contains("a")) { 
+        addSeries(_table, "a", currentFragmentIonTypes.getCharge("a"), new Fragment(bFragmentMz - FragmentIonTableV2.CO, position, nterPtmlNLMasse))
+        addSeries(_table, "a*", currentFragmentIonTypes.getCharge("a"), new Fragment(bFragmentMz - FragmentIonTableV2.CO - FragmentIonTableV2.NH3, position, nterPtmlNLMasse))
+        addSeries(_table, "a0", currentFragmentIonTypes.getCharge("a"), new Fragment(bFragmentMz - FragmentIonTableV2.CO - FragmentIonTableV2.H2O, position, nterPtmlNLMasse))
+      }
+
+      if (currentFragmentIonTypes.contains("b")) {
+        addSeries(_table, "b", currentFragmentIonTypes.getCharge("b"), new Fragment(bFragmentMz, position, nterPtmlNLMasse))
+        addSeries(_table, "b*", currentFragmentIonTypes.getCharge("b"), new Fragment(if (nlResidues("NH3")(0) > 0) bFragmentMz - FragmentIonTableV2.NH3 else 0.0, position, nterPtmlNLMasse))
+        addSeries(_table, "b0", currentFragmentIonTypes.getCharge("b"), new Fragment(if (nlResidues("H2O")(0) > 0) bFragmentMz - FragmentIonTableV2.H2O else 0.0, position, nterPtmlNLMasse))
+      }
+      
+      if (currentFragmentIonTypes.contains("c")) addSeries(_table, "c", currentFragmentIonTypes.getCharge("c"), new Fragment(bFragmentMz + FragmentIonTableV2.NH3, position, nterPtmlNLMasse))
+
+      if (currentFragmentIonTypes.contains("y")) {
+        addSeries(_table, "y", currentFragmentIonTypes.getCharge("y"), new Fragment(yFragmentMz, reversePosition, cterPtmNLMasse))
+        addSeries(_table, "y*", currentFragmentIonTypes.getCharge("y"), new Fragment(if (nlResidues("NH3")(1) > 0) yFragmentMz - FragmentIonTableV2.NH3 else 0.0, reversePosition, cterPtmNLMasse))
+        addSeries(_table, "y0", currentFragmentIonTypes.getCharge("y"), new Fragment(if (nlResidues("H2O")(1) > 0) yFragmentMz - FragmentIonTableV2.H2O else 0.0, reversePosition, cterPtmNLMasse))
+      }
+
+      if (currentFragmentIonTypes.contains("x"))  addSeries(_table, "x", currentFragmentIonTypes.getCharge("x"), new Fragment(yFragmentMz + FragmentIonTableV2.CO, reversePosition, cterPtmNLMasse))
+      if (currentFragmentIonTypes.contains("z"))  addSeries(_table, "z", currentFragmentIonTypes.getCharge("z"), new Fragment(yFragmentMz - FragmentIonTableV2.NH3, reversePosition, cterPtmNLMasse))
+
+      position += 1 
+      reversePosition -= 1
+    }
+
+    position += 1
+    // initialize last forward ions fragments to 0.0
+    if (currentFragmentIonTypes.contains("b")) {
+      addSeries(_table, "b", currentFragmentIonTypes.getCharge("b"), new Fragment(0.0, position, nterPtmlNLMasse))
+      addSeries(_table, "b*", currentFragmentIonTypes.getCharge("b"), new Fragment(0.0, position, nterPtmlNLMasse))
+      addSeries(_table, "b0", currentFragmentIonTypes.getCharge("b"), new Fragment(0.0, position, nterPtmlNLMasse))
+    }
+    if (currentFragmentIonTypes.contains("a")) {
+      addSeries(_table, "a", currentFragmentIonTypes.getCharge("a"), new Fragment(0.0, position, cterPtmNLMasse))
+      addSeries(_table, "a*", currentFragmentIonTypes.getCharge("a"), new Fragment(0.0, position, cterPtmNLMasse))
+      addSeries(_table, "a0", currentFragmentIonTypes.getCharge("a"), new Fragment(0.0, position, cterPtmNLMasse))
+    }
+    if (currentFragmentIonTypes.contains("c")) addSeries(_table, "c", currentFragmentIonTypes.getCharge("c"), new Fragment(0.0, position, cterPtmNLMasse))
+
+    // generate alternate NL fragments
+    if (peptide.ptms != null) { 
+    for (seriesName <- _table.keys) {
+      val isReverse = Fragmentation.isReverseSeries(seriesName)
+      for (fragment <- _table(seriesName).filter(_.moz > 0.0)) {
+        val includedPtms = if (!isReverse) peptide.ptms.filter(_.seqPosition <= fragment.position) else peptide.ptms.filter(aaSequence.length - _.seqPosition <= (fragment.position-1))
+        val includedPtmsByDefinition = includedPtms.groupBy(_.definition)
+        for (ptmDef <- includedPtmsByDefinition.keys) {
+          for (nl <- ptmDef.neutralLosses) {
+            if ((nl.monoMass - fragment.neutralLoss).abs > MathUtils.EPSILON_FLOAT) {
+              // Add a new fragment 
+              addFragment(_table, seriesName, new Fragment( 
+            		  moz = fragment.moz - includedPtmsByDefinition(ptmDef).size*(nl.monoMass - fragment.neutralLoss)/fragment.charge.toDouble , 
+            		  position = fragment.position, 
+            		  neutralLoss = includedPtmsByDefinition(ptmDef).size*nl.monoMass,
+            		  fragmentType = fragment.fragmentType,
+            		  series = fragment.series, 
+            		  charge = fragment.charge) )
+            }
+          }
+        }
+      }
+    }
+    }
+    
+    _table
+  }
+
+  private val table: Array[TheoreticalFragmentSeries] = {
+    val fragmentIonTable = new ArrayBuffer[TheoreticalFragmentSeries]
+    try {
+      for (item <- fragments) {
+        if (item._2.exists { _.moz > 0.0 }) fragmentIonTable += new TheoreticalFragmentSeries(item._1, item._2.map(_.moz).toArray)
+      }
+    } catch {
+      case e: Exception => logger.warn("The fragment ion table could not be generated, default value is 'new ArrayBuffer[TheoreticalFragmentSeries]'")
+    }
+    fragmentIonTable.toArray
+  }
+
+  def get: Array[TheoreticalFragmentSeries] = table
+
+  private def addFragment(_table: HashMap[String, ArrayBuffer[Fragment]], serie: String, fragment: Fragment) {
+    if (!_table.contains(serie)) _table.put(serie, new ArrayBuffer[Fragment])
+    _table(serie) += fragment
+  }
+
+  private def addSeries(_table: HashMap[String, ArrayBuffer[Fragment]], seriePrefix: String, charge: Int, fragment: Fragment) {
+    var serie = seriePrefix
+    addFragment(_table, serie, new Fragment( 
+            moz = fragment.moz, 
+            position = fragment.position, 
+            neutralLoss = fragment.neutralLoss,
+            fragmentType = fragment.fragmentType,
+            series = Some(serie),
+            charge = 1))
+    // neutralLossTable.update(serie, fragmentIon.getNumber(), sumNeutralLossMasses(fragmentIon))
+    if (charge > 1) {
+      serie += "+" // "+" is not indicated for singly charged ion (implicit)
+      // for each charge in the serie (ie. b, b++, b+++), add the corresponding moz
+      for (c <- 2 to charge) {
+        serie += "+"
+        addFragment(_table, serie, new Fragment( 
+            moz = { if (fragment.moz > 0) (fragment.moz + (c - 1) * MolecularConstants.PROTON_MASS) / c else 0.0 }, 
+            position = fragment.position, 
+            neutralLoss = fragment.neutralLoss,
+            fragmentType = fragment.fragmentType,
+            series = Some(serie),
+            charge = 2))
+      }
+    }
+  }
+
+  override def toString: String = FragmentIonTableV2.fragmentIonTableAsString(table)
+
+}
---
+++ pm-fragmentmatch-generator/src/main/scala/fr/proline/module/fragment_match/PeptideSpectrumMatcher.scala
@@ -0,0 +1,196 @@
+package fr.proline.module.fragment_match
+
+import scala.Array.canBuildFrom
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.StringBuilder
+import scala.util.control.Breaks.break
+import scala.util.control.Breaks.breakable
+import com.typesafe.scalalogging.slf4j.Logging
+import fr.proline.core.om.model.msi.FragmentMatch
+import fr.proline.core.om.model.msi.FragmentMatchType
+import fr.proline.core.om.model.msi.LocatedPtm
+import fr.proline.core.om.model.msi.PeptideMatch
+import fr.proline.core.om.model.msi.Spectrum
+import fr.proline.core.om.model.msi.SpectrumMatch
+import fr.proline.util.ms.MassTolUnit
+import fr.proline.util.ms.calcMozTolInDalton
+
+case class Peak(moz: Double, intensity: Float)
+
+class PeptideSpectrumMatcher(val spectraByIds: Map[Long, Spectrum],
+  val ms2ErrorTol: Double,
+  val ms2ErrorTolUnitStr: String) extends Logging {
+
+  def getSpectrumMatch(peptideMatch: PeptideMatch): SpectrumMatch = {
+    val ms2ErrorTolUnit = MassTolUnit.withName(ms2ErrorTolUnitStr)
+    val LabelRegex = """([^\+]+)([\w\+]*)""".r      
+    
+    var start = System.currentTimeMillis()
+
+    val spectrum = spectraByIds(peptideMatch.getMs2Query.spectrumId)
+    val allPeaks = for ( (m,i) <- spectrum.mozList.get.zip(spectrum.intensityList.get)) yield new Peak(moz = m, intensity = i)
+    val usedPeaksCount = {
+      if (peptideMatch.properties.isDefined && peptideMatch.properties.get.mascotProperties.isDefined) {
+        peptideMatch.properties.get.mascotProperties.get.getUsedPeaksCount.getOrElse(allPeaks.length)
+      } else { allPeaks.length }
+    }
+
+    var usedPeaks = mascotLikePeaksSelection(peptideMatch, allPeaks, usedPeaksCount)
+    usedPeaks = usedPeaks.sortBy(_.moz)
+
+    //TODO : configure ion series used
+    val nlString = {
+      if (peptideMatch.properties.isDefined && peptideMatch.properties.get.mascotProperties.isDefined) {
+        peptideMatch.properties.get.mascotProperties.get.getNlString.getOrElse("");
+      } else { "" }
+    }
+    
+    val aaSequence = {
+      val seq = peptideMatch.peptide.sequence.toCharArray()
+      if (peptideMatch.properties.isDefined && peptideMatch.properties.get.mascotProperties.isDefined && peptideMatch.properties.get.mascotProperties.get.getAmbiguityString.isDefined) {
+        val ambiguities = peptideMatch.properties.get.mascotProperties.get.getAmbiguityString.get.split(",");
+        for (i <- 0 to (ambiguities.length-1) by 3) {
+        	seq(ambiguities(i).toInt - 1) = ambiguities(i+2).charAt(0)
+        } 
+      } 
+      seq
+    }
+    
+    val ptmNeutralLosses = new HashMap[LocatedPtm, Double]()
+    for (idx <- 0 until nlString.size) {
+    	if(nlString.charAt(idx) != '0') {
+    	  val ptm = peptideMatch.peptide.ptms.find(_.seqPosition == (idx))
+    	  ptmNeutralLosses += (ptm.get -> ptm.get.definition.neutralLosses(nlString.charAt(idx).asDigit - 1).monoMass)
+    	}
+    }
+    val charge = scala.math.min(peptideMatch.msQuery.charge, 2)
+    val currentFragmentIonTypes = new FragmentIons(ionTypeB = true, ionTypeY = true, chargeForIonsB = charge, chargeForIonsY = charge)
+    val theoreticalFragmentsTable = new FragmentIonTableV2(peptideMatch.peptide, currentFragmentIonTypes, Some(aaSequence), ptmNeutralLosses = if (ptmNeutralLosses.isEmpty) None else Some(ptmNeutralLosses.toMap))
+
+    val theoFragments = theoreticalFragmentsTable.fragments.map(_._2).flatten
+    val fragMatches = new ArrayBuffer[FragmentMatch]()
+
+    for (fragment <- theoFragments) {
+
+      val theoFragMoz = fragment.moz
+      val mozTolInDa = calcMozTolInDalton(theoFragMoz, ms2ErrorTol, ms2ErrorTolUnit)
+      var bestMatch: FragmentMatch = null
+
+      breakable {
+        for (ms2Peak <- usedPeaks) {
+
+          val obsMoz = ms2Peak.moz
+          val deltaMoz = obsMoz - theoFragMoz
+
+          if (scala.math.abs(deltaMoz) <= mozTolInDa) {
+
+            val LabelRegex(ionserie, chargeStr) = fragment.series.get
+            val label = new StringBuilder().append(ionserie).append('(').append(fragment.position.toString).append(')').append(chargeStr)
+            if (fragment.neutralLoss > 0.0) {
+              label.append(" -"+scala.math.round(fragment.neutralLoss))
+            }
+            
+            val fragMatch = new FragmentMatch(
+              label = label.toString,
+              `type` = fragment.fragmentType.collect { case x: FragmentMatchType.Value => x.toString },
+              moz = obsMoz,
+              calculatedMoz = theoFragMoz,
+              intensity = ms2Peak.intensity)
+
+            val nl = fragment.neutralLoss
+            if (nl > 0.0) fragMatch.neutralLossMass = Some(nl)
+
+            if ((bestMatch == null) || (bestMatch.intensity < fragMatch.intensity)) {
+              bestMatch = fragMatch
+            }
+
+          } else if (scala.math.signum(deltaMoz) > 0) break
+        }
+      }
+
+      if (bestMatch != null) fragMatches += bestMatch
+    }
+
+
+    new SpectrumMatch(peptideMatch.getMs2Query.initialId, peptideMatch.rank, theoreticalFragmentsTable.get, fragMatches.toArray)
+
+  }
+  
+  def mascotLikePeaksSelection(peptideMatch: PeptideMatch, allPeaks: Array[Peak], usedPeaksCount: Int): ArrayBuffer[Peak] = {
+    
+    var usedPeaks = new ArrayBuffer[Peak](usedPeaksCount + 2)
+    val allPeaksSorted = allPeaks.sortBy(_.moz)
+
+    if (usedPeaksCount != allPeaks.length) {
+
+      var usedPeaks2 = new ArrayBuffer[Peak](usedPeaksCount)
+      var allPeaksBinned = new ArrayBuffer[ArrayBuffer[Peak]]
+      var refMz = allPeaksSorted.head.moz
+      allPeaksBinned += new ArrayBuffer[Peak]
+      for (i <- 0 until allPeaksSorted.length) {
+        if (allPeaksSorted(i).moz < (refMz + 100)) {
+          allPeaksBinned.last += allPeaksSorted(i)
+        } else {
+          allPeaksBinned(allPeaksBinned.size - 1) = allPeaksBinned.last.sortBy(_.intensity).reverse
+          allPeaksBinned += new ArrayBuffer[Peak]
+          allPeaksBinned.last += allPeaksSorted(i)
+          do {
+            refMz += 100
+          } while ((refMz + 100) < allPeaksSorted(i).moz)
+        }
+      }
+      // sort the last bin
+      allPeaksBinned(allPeaksBinned.size -1) = allPeaksBinned.last.sortBy(_.intensity).reverse
+      var rank = 0
+      while (usedPeaks2.length < usedPeaksCount) {
+        for (pb <- allPeaksBinned) {
+          if (rank < pb.length && pb(rank).moz < peptideMatch.peptide.calculatedMass) {
+            usedPeaks2 += pb(rank)
+          }
+        }
+        rank += 1
+      }
+
+      usedPeaks += Peak(0.0, 0f)
+      usedPeaks ++= usedPeaks2.sortBy(_.moz)
+      usedPeaks += Peak(Double.MaxValue, 0f)
+
+    
+    // Define some vars
+    var prevPeak: Peak = null
+    var j = 0
+
+    // Iterate over matching peaks
+    for (i <- 0 until usedPeaks.length) {
+      val peak = usedPeaks(i)
+      var intThresh = peak.intensity
+
+      // TODO: explain this computation
+      if ((i > 0) && (i < (usedPeaks.length - 1)) && (prevPeak.intensity < peak.intensity)) {
+        intThresh = prevPeak.intensity
+      }
+
+      while ((j < allPeaksSorted.length) && (allPeaksSorted(j).moz < peak.moz)) {
+        val newPeak = allPeaksSorted(j)
+        if (newPeak.intensity >= intThresh) {
+          usedPeaks += newPeak
+        }
+        j += 1
+      }
+      j += 1
+
+      prevPeak = peak
+    }
+      
+      } else {
+      // Fill usedPeaks with firstSortedPeaks data  
+      usedPeaks += Peak(0.0, 0f)
+      usedPeaks ++= allPeaksSorted
+      usedPeaks += Peak(Double.MaxValue, 0f)
+    }
+    //removes marker peaks at moz 0.0 and Double.MaxValue
+    usedPeaks.filter(p => (p.moz > 0.0 && p.moz < Double.MaxValue))
+  }
+
+}
