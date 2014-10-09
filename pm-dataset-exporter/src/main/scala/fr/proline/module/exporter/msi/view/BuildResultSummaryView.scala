d16a8e14c4ab90ea540f60804aa8c4557de8f939
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-10-09 14:42:16 +0000

    Redefinition des vues et propriétés accessible dans IdentDataSet

--- pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/msi/view/BuildResultSummaryView.scala
+++ pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/msi/view/BuildResultSummaryView.scala
@@ -5,6 +5,8 @@ import scala.collection.mutable.HashSet
 import fr.proline.core.om.model.msi.ResultSummary
 import fr.proline.module.exporter.api.view.IDataView
 import fr.proline.module.exporter.api.view.IViewTypeEnumeration
+import scala.collection.mutable.ArrayBuffer
+import fr.proline.core.om.model.msi.ProteinMatch
 
 case class IdentDataSet(
   projectName: String,
@@ -24,10 +26,25 @@ case class IdentDataSet(
   ) {
     protSetIdSetByPepMatchId.getOrElseUpdate(pepMatchId, new HashSet[Long] ) += protSet.id
     protMatchIdSetByPepMatchId.getOrElseUpdate(pepMatchId, new HashSet[Long] ) += protMatchId      
+    
   }
   
   lazy val pepMatchById = resultSummary.resultSet.get.peptideMatchById
-
+  
+  //   Create list of all ProtMatches for pepMatches (validated or not) 
+  lazy val allProtMatchSetByPepId =  { 
+    val resultBuilder = new HashMap[Long, HashSet[ProteinMatch]]
+    for( protMatch <- resultSummary.resultSet.get.proteinMatches ) {
+      val protMatchPeptMatches = new ArrayBuffer[Long]()
+      if (protMatch.sequenceMatches != null) {
+        protMatch.sequenceMatches.foreach(seqMatch => {
+          val pepId = seqMatch.getPeptideId
+          resultBuilder.getOrElse(seqMatch.getPeptideId, new HashSet[ProteinMatch]) +=protMatch          
+        }) //end go throudh protMatch sequenceMatches 
+      }      
+    } //End go through ProtMarches
+    resultBuilder
+  }
 }
 
 object BuildResultSummaryView {
@@ -39,8 +56,9 @@ object BuildResultSummaryView {
     ResultSummaryViewTypes.PEP_SET_TO_PROT_MATCH -> { ds: IdentDataSet => new PepSetToProtMatchView(ds) },
     ResultSummaryViewTypes.PROT_SET_TO_PROT_MATCH -> { ds: IdentDataSet => new ProtSetToProtMatchView(ds) },
     ResultSummaryViewTypes.PROT_SET_TO_TYPICAL_PROT_MATCH -> { ds: IdentDataSet => new ProtSetToTypicalProtMatchView(ds) },
-    ResultSummaryViewTypes.PROT_SET_TO_BEST_PEPTIDE_MATCH -> { ds: IdentDataSet => new ProtSetToBestPepMatchView(ds) },    
-    ResultSummaryViewTypes.ALL_PEPTIDE_MATCHES -> { ds: IdentDataSet => new AllPeptideMatchesView(ds) }
+    ResultSummaryViewTypes.PROT_SET_TO_BEST_PEPTIDE_MATCH -> { ds: IdentDataSet => new ProtSetToBestPepMatchView(ds) },
+    ResultSummaryViewTypes.ALL_PEPTIDE_MATCHES -> { ds: IdentDataSet => new AllPeptideMatchesView(ds) },
+    ResultSummaryViewTypes.ALL_PROT_SET_PEPTIDE_MATCHES -> { ds: IdentDataSet => new AllProtSetPSMView(ds) }
   )
 
   def apply( identDS: IdentDataSet, viewType: IViewTypeEnumeration#Value ): IDataView = {
