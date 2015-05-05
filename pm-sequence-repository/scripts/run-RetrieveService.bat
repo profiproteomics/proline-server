f4f63ab1669bce5e7cb542b3dcad245820df44af
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-05-05 13:14:24 +0000

    Use 'config' in classPath. Directory created by goal assembly single to create distrib zip

--- pm-sequence-repository/scripts/run-RetrieveService.bat
+++ pm-sequence-repository/scripts/run-RetrieveService.bat
@@ -1,1 +1,1 @@
-java -cp "classes;PM-SequenceRepository-${project.version}.jar;lib\*" fr.proline.module.seq.service.RetrieveService 2
+java -cp "config;PM-SequenceRepository-${project.version}.jar;lib\*" fr.proline.module.seq.service.RetrieveService 2
--- pm-sequence-repository/scripts/run-RetrieveService.sh
+++ pm-sequence-repository/scripts/run-RetrieveService.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "classes:PM-SequenceRepository-${project.version}.jar:lib/*" fr.proline.module.seq.service.RetrieveService 2
+java -cp "config:PM-SequenceRepository-${project.version}.jar:lib/*" fr.proline.module.seq.service.RetrieveService 2
