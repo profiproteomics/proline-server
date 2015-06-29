bcee258af353ddc9e292134de3be8ec2a5e6dabb
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-06-29 09:51:53 +0000

    #12859 : Search specifically for unimod section. Not the 4th one.

---
+++ pm-mascot-parser/src/test/scala/fr/proline/module/parser/mascot/MascotPTMDefiitionTest.scala
@@ -0,0 +1,55 @@
+package fr.proline.module.parser.mascot
+
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import com.typesafe.scalalogging.slf4j.Logging
+import java.io.File
+import org.junit.Assert
+
+/**
+ * @author VD225637
+ *
+ */
+@Test
+class MascotPTMDefiitionTest extends Logging {
+
+	@Before
+  def init() {
+    logger.debug("Start Logging MascotPTMDefiitionTest")
+    
+  } 
+
+  @After
+  def tearDown() {
+   
+  }
+  
+  @Test
+  def testReadModif() = {
+	  val filename = "/dat_samples/GRE_F068213_M2.4_TD_EColi.dat"
+	  val f = new File(this.getClass().getResource(filename).toURI())
+	  val mascotProvider = new MascotResultFileProvider()
+	  val result = mascotProvider.getPtmDefinitions(f, null)
+	  Assert.assertNotNull(result)
+	  Assert.assertEquals(29, result.length)
+	  val ptmNames = result.groupBy(_.names).keySet
+	  Assert.assertEquals(3, ptmNames.size)
+  }
+
+    
+  @Test
+  def testReadModifInFifthPosoition() = {	  
+	  val filename = "/dat_samples/header_unimodFifth_F063872.dat"
+	  val f = new File(this.getClass().getResource(filename).toURI())
+	  val mascotProvider = new MascotResultFileProvider()
+	  val result = mascotProvider.getPtmDefinitions(f, null)
+	  Assert.assertNotNull(result)
+	  Assert.assertEquals(19, result.length)
+	  val ptmNames = result.groupBy(_.names).keySet
+	  Assert.assertEquals(2, ptmNames.size)
+  
+  }
+
+  
+}
