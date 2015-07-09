8e7733823b48ea918d9f9012448d1dde11420772
romdhani1987 <romdhani@live.fr>
2015-07-09 14:15:24 +0000

    params of validation

---
+++ pm-sequence-repository/src/main/java/fr/proline/module/seq/orm/Params.java
@@ -0,0 +1,10 @@
+package fr.proline.module.seq.orm;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Params {
+	public List<Peptidefilters> peptide_filters = new ArrayList<Peptidefilters>();
+	public List<proteinfilters> protein_filters =new ArrayList<proteinfilters>();
+}
