363354ad55e11646fe15ffe763766b3124bb2d34
romdhani1987 <romdhani@live.fr>
2015-07-09 14:14:45 +0000

    add list of Proprieties

---
+++ pm-sequence-repository/src/main/java/fr/proline/module/seq/orm/Proprieties.java
@@ -0,0 +1,12 @@
+package fr.proline.module.seq.orm;
+
+public class Proprieties {
+ public  ValidationProperties validation_properties;
+
+public ValidationProperties getValidation_properties() {
+	return validation_properties;
+}
+public void setValidation_properties(ValidationProperties validation_properties) {
+	this.validation_properties = validation_properties;
+}
+}
