93910c725dafd0f5e2578abbc2b255dedecdd731
romdhani1987 <romdhani@live.fr>
2015-07-09 14:22:11 +0000

    get the  Properties of validation

---
+++ pm-sequence-repository/src/main/java/fr/proline/module/seq/orm/Properties.java
@@ -0,0 +1,12 @@
+package fr.proline.module.seq.orm;
+
+public class Properties {
+public String threshold_value;
+
+public String getThreshold_value() {
+	return threshold_value;
+}
+public void setThreshold_value(String threshold_value) {
+	this.threshold_value = threshold_value;
+}
+}
