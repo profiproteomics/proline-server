da946263c695d60cb7c7bb3f1b163b81f4b4b2d0
romdhani1987 <romdhani@live.fr>
2015-07-09 14:13:18 +0000

    Add Validation proprities

---
+++ pm-sequence-repository/src/main/java/fr/proline/module/seq/orm/ValidationProperties.java
@@ -0,0 +1,20 @@
+package fr.proline.module.seq.orm;
+
+public class ValidationProperties {
+	public Params params;
+	public Results results;
+	
+	public Params getParams() {
+		return params;
+	}
+	public void setParams(Params params) {
+		this.params = params;
+	}
+	public Results getResults() {
+		return results;
+	}
+	public void setResults(Results results) {
+		this.results = results;
+	}
+
+}
