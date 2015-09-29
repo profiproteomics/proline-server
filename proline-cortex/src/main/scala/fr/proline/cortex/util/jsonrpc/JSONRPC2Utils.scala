7849bbcb76960f5f24eadca72b9d94a250495f22
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-09-29 06:21:45 +0000

    Autorise les paramètres null

--- proline-cortex/src/main/scala/fr/proline/cortex/util/jsonrpc/JSONRPC2Utils.scala
+++ proline-cortex/src/main/scala/fr/proline/cortex/util/jsonrpc/JSONRPC2Utils.scala
@@ -5,12 +5,17 @@ import com.thetransactioncompany.jsonrpc2.util.NamedParamsRetriever
 
 object JSONRPC2Utils {
 
+  /***
+   * Return NamedParamsRetriever specified in JSONRPC2Request or null if none was specified
+   */
   def buildParamsRetriever(req: JSONRPC2Request): NamedParamsRetriever = {
     require((req != null), "Req is null")
 
     val namedParams = req.getNamedParams
-
-    new NamedParamsRetriever(namedParams)
+    if(namedParams !=null)
+    	new NamedParamsRetriever(namedParams)
+    else
+      null
   }
 
 }
