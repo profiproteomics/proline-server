b6c5d58635207582360296d7b9e245af5ae7851c
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-01-05 15:03:33 +0000

    #13928 : No link between SEDB and Parsing Rule (usage will be modified, only for information)

--- pm-sequence-repository/src/main/java/fr/proline/module/seq/orm/SEDb.java
+++ pm-sequence-repository/src/main/java/fr/proline/module/seq/orm/SEDb.java
@@ -32,10 +32,11 @@ public class SEDb implements Serializable {
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Alphabet alphabet;
-
-	@ManyToOne
-	@JoinColumn(name = "parsing_rule_id")
-	private ParsingRule parsingRule;
+//
+	//VDS : TODO Use map, may have many Parsing Rule ... or save last one ?! 
+//	@ManyToOne
+//	@JoinColumn(name = "parsing_rule_id")
+//	private ParsingRule parsingRule;
 
 	@ManyToOne
 	@JoinColumn(name = "repository_id")
@@ -66,13 +67,13 @@ public class SEDb implements Serializable {
 		return alphabet;
 	}
 
-	public void setParsingRule(final ParsingRule pParsingRule) {
-		parsingRule = pParsingRule;
-	}
-
-	public ParsingRule getParsingRule() {
-		return parsingRule;
-	}
+//	public void setParsingRule(final ParsingRule pParsingRule) {
+//		parsingRule = pParsingRule;
+//	}
+//
+//	public ParsingRule getParsingRule() {
+//		return parsingRule;
+//	}
 
 	public void setRepository(final Repository pRepository) {
 		repository = pRepository;
