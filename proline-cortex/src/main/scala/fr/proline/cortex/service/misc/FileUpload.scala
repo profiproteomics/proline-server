2223800ba925ae742e828623333ca3c53942d20d
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-05-30 13:44:29 +0000

    new FileUpload service (use JMS API )

--- proline-cortex/src/main/scala/fr/proline/cortex/service/misc/FileUpload.scala
+++ proline-cortex/src/main/scala/fr/proline/cortex/service/misc/FileUpload.scala
@@ -1,97 +1,73 @@
 package fr.proline.cortex.service.misc
 
-import fr.proline.jms.service.api.IRemoteBytesMessageService
-import com.typesafe.scalalogging.LazyLogging
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import scala.util.control.Breaks._
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Error
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response
-import fr.proline.jms.util.jsonrpc.JSONRPC2Utils
+import com.typesafe.scalalogging.LazyLogging
 import fr.proline.cortex.util.MountPointRegistry
+import fr.proline.jms.service.api.IRemoteBytesMsgService
 import fr.proline.jms.util.jsonrpc.ProfiJSONRPC2Response
 import javax.jms.BytesMessage
-import fr.proline.jms.ServiceRunner
-import java.io.FileOutputStream
 import fr.proline.jms.util.JMSConstants
-import java.io.BufferedOutputStream
-import java.io.File
-import scala.util.control.Breaks._
+import fr.proline.cortex.util.WorkDirectoryFactory
+import fr.proline.jms.ServiceRunner
 
-class FileUpload extends IRemoteBytesMessageService with LazyLogging {
+/**
+ * 
+ * Define JMS Service to get Upload a file on Proline Server FileSystem.
+ * 
+ * This is not a JSON RPC Service. The unique parameter is specified in the message header.
+ * -  dest_file_name : name of the file which will be uploaded on server FileSystem in 
+ * working directory.
+ * 
+ * 
+ */
+class FileUpload extends IRemoteBytesMsgService with LazyLogging {
 
   /* JMS Service identification */
   val serviceName = "proline/misc/FileUpload"
   val serviceVersion = "1.0"
   override val defaultVersion = true
 
-  val DEST_FOLDER_PATH_PARAM_KEY = "dest_folder_path"
+//  val DEST_FOLDER_PATH_PARAM_KEY = "dest_folder_path"
   val DEST_FILE_NAME_PARAM_KEY = "dest_file_name"
 
-  override def service(jmsMessageContext: Map[String, Any], req: JSONRPC2Request): JSONRPC2Response = {
-
-    return new JSONRPC2Response(JSONRPC2Error.INVALID_REQUEST, req.getID)
-  }
-
-  override def service(jmsMessageContext: Map[String, Any], message: BytesMessage, req: JSONRPC2Request): JSONRPC2Response = {
-    require((req != null), "Req is null")
+  override def service(jmsMessageContext: Map[String, Any], message: BytesMessage): JSONRPC2Response = {
     require((message != null), "message is null")
 
-    val requestId = req.getID
-    val methodName = req.getMethod
-
-    /* Method dispatch */
-    methodName match {
-       case "process" => { upload(jmsMessageContext,message,  req) }
-
-      // Method name not supported
-      case _ => return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, requestId)
-    }
-  }
-  
-  private def upload(jmsMessageContext: Map[String, Any],message: BytesMessage, req: JSONRPC2Request): JSONRPC2Response = {
-    assert((req != null), "upload() req is null")
-
-    val jsonRequestId = req.getID
+    val jsonRequestId = message.getJMSMessageID
     var jsonResponse: JSONRPC2Response = null
-    val mountsPoints = MountPointRegistry.retrieveMountPointsByType(MountPointRegistry.RESULT_FILES_DIRECTORY, true)
-    var destFileName = ""
-    breakable { 
-      for(mp <- mountsPoints){
-        if(mp.label.endsWith("VDTEST")){
-          destFileName = mp.path
-          break
-        }
-      }
-    }
     
+    val  tmpDirPath =  WorkDirectoryFactory.prolineWorkDirectory.getAbsolutePath
+        
+    val dirPath = new File(tmpDirPath)
+   
+    if (!dirPath.isDirectory()) {
+      jsonResponse = new JSONRPC2Response(ServiceRunner.buildJSONRPC2Error(JMSConstants.SERVICE_ERROR_CODE, "Invalid destination folder "+tmpDirPath), jsonRequestId)
 
-    val localPathname = destFileName+"/vdtest.txt"
-    val destFile = new File(localPathname);
-//    if (!destFile.exists())
-//      destFile.mkdirs()
+    } else {
 
-//    if (!destFolder.isDirectory()) {
-//      jsonResponse = new JSONRPC2Response(ServiceRunner.buildJSONRPC2Error(JMSConstants.SERVICE_ERROR_CODE, "Invalid \"" + DEST_FOLDER_PATH_PARAM_KEY + "\" JSON-RPC named param"), jsonRequestId)
-//
-//    } else {
-
-    //ReadFile name
-     val fName = message.readUTF()
-     logger.info("READ "+fName)
-//      val file = new File(destFolder, fileName);
+      //Read File name
+      val readFileName =   message.getStringProperty(DEST_FILE_NAME_PARAM_KEY)
+      logger.debug("READ dest File "+readFileName)
+      val destFile = new File(dirPath, readFileName)
+//      val destFile = File.createTempFile("uploaded_",".proTmp",  dirPath)
+//      destFile.deleteOnExit()
+       
       val fos: FileOutputStream = new FileOutputStream(destFile);
       val outBuf: BufferedOutputStream = new BufferedOutputStream(fos);
-      var nbrByte: Integer = 0;
-      var i: Int = message.readInt()
-      while (!i.equals(-1)) {
-        outBuf.write(i);
-        nbrByte = nbrByte + 4 //read more 4byte
-        i = message.readInt()
-      }
+      message.setObjectProperty("JMS_HQ_SaveStream", outBuf)
+
+
       outBuf.close()
       fos.close();
 
-      jsonResponse = new ProfiJSONRPC2Response(nbrByte, jsonRequestId)
-    
+      jsonResponse = new ProfiJSONRPC2Response(destFile.getAbsolutePath, jsonRequestId)
+    }
 
     jsonResponse
   }
