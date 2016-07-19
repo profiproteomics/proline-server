ebef218e5c3006ef66d080ef5f06699b2b865a88
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-07-19 13:57:38 +0000

    Revert move

--- proline-cortex-api/src/main/scala/fr/proline/cortex/api/service/admin/IAdminService.scala
+++
@@ -1,8 +0,0 @@
-package fr.proline.cortex.api.service.admin
-
-import fr.proline.cortex.api.service.dps.IDataProcessingService
-import fr.proline.cortex.api.JSONRPC2Service
-
-trait IAdminService extends JSONRPC2Service {
-  val serviceNamespace = "proline/admin"
-}
--- proline-cortex-api/src/main/scala/fr/proline/cortex/api/service/dps/msi/IMsiService.scala
+++
@@ -1,7 +0,0 @@
-package fr.proline.cortex.api.service.dps.msi
-
-import fr.proline.cortex.api.service.dps.IDataProcessingService
-
-trait IMsiService extends IDataProcessingService {
-  val serviceNamespace = "proline/dps/msi"
-}
