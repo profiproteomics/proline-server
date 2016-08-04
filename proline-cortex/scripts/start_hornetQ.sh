1ec879f09b7755055f23b0b2ed29a5e261ea1970
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-08-04 14:33:40 +0000

    Allow 2 assembly : Cortex only or full (cortex + hornetQ)

---
+++ proline-cortex/scripts/start_hornetQ.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#------------------------------------------------
+# Simple shell-script to run HornetQ Cortex Ready standalone server
+#------------------------------------------------
+cd ./hornetq_light-2.4.0.Final/bin
+chmod +x run.sh
+./run.sh
