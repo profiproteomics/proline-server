d06e50f4acd44b57797d6d7c000800a65d469206
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-09-06 12:35:14 +0000

    #15386 : change la valeur par défaut pour la liste des protSet à exporter : uniquement les validés

--- pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/commons/config/ExportConfigData.scala
+++ pm-dataset-exporter/src/main/scala/fr/proline/module/exporter/commons/config/ExportConfigData.scala
@@ -2,22 +2,18 @@ package fr.proline.module.exporter.commons.config
 
 /**
  * represents which data to export
- * for now, only which protein to export (all or validated proteins)
+ * for now, 
+ * - which protein (all or validated proteins[default])
+ * - which quant peptide (from best profile[default] or all profile)
+ * to export 
  */
 case class ExportConfigData(
-  allProteinSet: Boolean = true,
+   //allProteinSet set to false :  RSM is validated result so by default, validated data exported. User may have incorrect result if this parameter is not changed -and is_validated not exported for warning-
+  allProteinSet: Boolean = false,
   bestProfile: Boolean = true // export  bestProfile or iterates over profiles
 )
 
 object ExportConfigData {
-
-  // get all config
-  /*def getAllConfig(): ExportConfigData = {
-    var dataConfig: ExportConfigData = new ExportConfigData()
-    dataConfig.allProteinSet = true
-    dataConfig.bestProfile = true
-    return dataConfig
-  }*/
-  
+ 
   def getDefaultConfig() = ExportConfigData()
 }
