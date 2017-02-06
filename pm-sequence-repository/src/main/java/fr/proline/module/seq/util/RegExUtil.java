dd43d263b0f867e79ccd8af99004fd8b41d8ff1d
Veronique Dupierris <veronique.dupierris@cea.fr>
2017-02-06 08:18:54 +0000

    Add getMatchingString method

--- pm-sequence-repository/src/main/java/fr/proline/module/seq/util/RegExUtil.java
+++ pm-sequence-repository/src/main/java/fr/proline/module/seq/util/RegExUtil.java
@@ -35,4 +35,30 @@ public class RegExUtil {
 		}
 		return result;
 	}
+	
+	/**
+	 * Use specified regex to extract searched text from source text 
+	 * 
+	 * @param sourceText : text to get searched text from
+	 * @param releaseRegEx : Regex to be used to get text from sourceText
+	 * @return extracted text or null if not found
+	 */
+	public static String getMatchingString(final String sourceText, final String searchStrRegEx) {
+		if(sourceText == null || searchStrRegEx == null)
+			return null;
+				
+		Pattern textPattern = Pattern.compile(searchStrRegEx, Pattern.CASE_INSENSITIVE);			
+
+		String result = null;
+		if(textPattern != null) {
+    		final Matcher matcher = textPattern.matcher(sourceText);
+    
+    		if (matcher.find()) {
+    
+    			if (matcher.groupCount() >= 1) 
+    				result = matcher.group(1).trim();
+    		} 
+		}
+		return result;
+	}
 }
