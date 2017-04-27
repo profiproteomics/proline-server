04b5d1be9e54fc99f8b5239f7da1a2bb60ebaa5b
David Bouyssié <David.Bouyssie@ipbs.fr>
2017-04-27 09:17:39 +0000

    [Proline-Cortex] Use the API of the CancelService instead of raw strings Add TODO: rename me to ServiceManager (because this service may have other methods)
    #16545

--- proline-cortex/src/main/scala/fr/proline/cortex/service/misc/CancelService.scala
+++ proline-cortex/src/main/scala/fr/proline/cortex/service/misc/CancelService.scala
@@ -5,54 +5,54 @@ import com.thetransactioncompany.jsonrpc2.JSONRPC2Request
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response
 import com.thetransactioncompany.jsonrpc2.util.NamedParamsRetriever
 import com.typesafe.scalalogging.LazyLogging
-import fr.profi.util.jsonrpc.IJSONRPC2Method
+
+import fr.profi.util.jsonrpc.BuildJSONRPC2Response
 import fr.profi.util.jsonrpc.JSONRPC2Utils
-import fr.proline.cortex.api.service.misc.IMiscService
+import fr.proline.cortex.api.service.misc._
 import fr.proline.jms.ServiceManager
 import fr.proline.jms.service.api.IDefaultServiceVersion
-import fr.proline.jms.util.JMSConstants
 import fr.proline.jms.service.api.IRemoteJsonRPC2Service
+import fr.proline.jms.util.JMSConstants
 
-class CancelService extends IMiscService with IDefaultServiceVersion with LazyLogging with IRemoteJsonRPC2Service   { 
+// TODO: rename me ServiceManager and implement multiple methods (cancel_service, get_service_status...)
+class CancelService extends ICancelService with IDefaultServiceVersion with LazyLogging with IRemoteJsonRPC2Service   { 
   
    /* JMS Service identification */
-  
-  val serviceLabel = "CancelService"
-  
-  // TODO: define me in Proline-Cortex-API
-  def methodDefinitions: Seq[IJSONRPC2Method] = Seq()
-
-  var requestID : Object = ""
   
   def runService(jsonRequest: JSONRPC2Request, jmsMessageContext: Map[String, Any]): JSONRPC2Response = {
     require(jsonRequest != null, "jsonRequest is null")
 
-    requestID = jsonRequest.getID
+    val requestID = jsonRequest.getID
     val methodName = jsonRequest.getMethod
 
     /* Method dispatch */
     methodName match {
-      case "cancel" => {
+      case CANCEL_METHOD.name => {
         val paramsRetriever = JSONRPC2Utils.buildParamsRetriever(jsonRequest)
-        return cancelProcess(jmsMessageContext, paramsRetriever)        
+        this._cancelProcess(paramsRetriever, requestID, jmsMessageContext)        
       }
       // Method name not supported
-      case _ => return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, requestID)
-      
-    } 
+      case _ => BuildJSONRPC2Response.forMethodNotFound(requestID)
+    }
   }
-  
-  def cancelProcess(jmsMessageContext: Map[String, Any], paramsRetriever: NamedParamsRetriever) : JSONRPC2Response = {
+
+  private def _cancelProcess(
+    paramsRetriever: NamedParamsRetriever,
+    requestID: Object,
+    jmsMessageContext: Map[String, Any]
+  ): JSONRPC2Response = {
+
     val cancelledMsgId = paramsRetriever.getString("message_id")
-    logger.info("Will Cancel message : "+cancelledMsgId)
+    logger.info("Will Cancel message : " + cancelledMsgId)
+
     val runningFuture = ServiceManager.getFutureForMessage(cancelledMsgId)
-    if(runningFuture != null ){
+    if (runningFuture != null) {
       val ret = runningFuture.cancel(true)
-      return new JSONRPC2Response(ret, requestID)
+      new JSONRPC2Response(ret, requestID)
     } else {
-        val errorMessage = s"Invalid Message ID ($cancelledMsgId)specified for 'Cancel' (##Message##_"+jmsMessageContext.get(JMSConstants.JMSMessageID)+")"
-        logger.warn(errorMessage)
-        new JSONRPC2Response(JSONRPC2Error.INVALID_PARAMS, errorMessage)
+      val errorMessage = s"Invalid Message ID ($cancelledMsgId)specified for 'Cancel' (##Message##_" + jmsMessageContext.get(JMSConstants.JMSMessageID) + ")"
+      logger.warn(errorMessage)
+      new JSONRPC2Response(JSONRPC2Error.INVALID_PARAMS, errorMessage)
     }
   }
 
