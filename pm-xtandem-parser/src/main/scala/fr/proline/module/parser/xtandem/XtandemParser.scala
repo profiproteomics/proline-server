cb464d4b7efa8d9b233374f06ed09fbd1118a773
AlexandreBurel <alexandre.burel@unistra.fr>
2017-05-18 12:44:15 +0000

    Fix for #16774: msiSearch object is now a lazy val

--- pm-xtandem-parser/src/main/scala/fr/proline/module/parser/xtandem/XtandemParser.scala
+++ pm-xtandem-parser/src/main/scala/fr/proline/module/parser/xtandem/XtandemParser.scala
@@ -1,50 +1,38 @@
 package fr.proline.module.parser.xtandem
 
 import java.io.File
-import fr.proline.core.om.provider.ProviderDecoratedExecutionContext
-import org.xml.sax.helpers.DefaultHandler
-import fr.proline.core.om.model.msi.IResultFile
-import com.typesafe.scalalogging.LazyLogging
-import fr.proline.core.om.model.msi.Spectrum
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import org.xml.sax.helpers.DefaultHandler
+import com.typesafe.scalalogging.LazyLogging
+import fr.profi.chemistry.model.MolecularConstants
+import fr.proline.core.om.model.msi.IResultFile
 import fr.proline.core.om.model.msi.IonTypes
-import fr.proline.core.om.model.msi.MsQuery
+import fr.proline.core.om.model.msi.LocatedPtm
 import fr.proline.core.om.model.msi.MSISearch
-import fr.proline.core.om.model.msi.MSMSSearchSettings
-import fr.proline.core.om.model.msi.Peaklist
-import fr.proline.core.om.model.msi.PtmLocation
-import fr.proline.core.om.model.msi.SpectrumMatch
+import fr.proline.core.om.model.msi.Ms2Query
+import fr.proline.core.om.model.msi.MsQuery
+import fr.proline.core.om.model.msi.Peptide
+import fr.proline.core.om.model.msi.PeptideMatch
+import fr.proline.core.om.model.msi.PeptideMatchProperties
+import fr.proline.core.om.model.msi.PeptideMatchScoreType
+import fr.proline.core.om.model.msi.PeptideMatchXtandemProperties
+import fr.proline.core.om.model.msi.Protein
+import fr.proline.core.om.model.msi.ProteinMatch
+import fr.proline.core.om.model.msi.PtmDefinition
 import fr.proline.core.om.model.msi.ResultSet
-import fr.proline.core.om.model.msi.SearchSettings
-import fr.proline.core.om.model.msi.SeqDatabase
+import fr.proline.core.om.model.msi.SequenceMatch
+import fr.proline.core.om.model.msi.Spectrum
+import fr.proline.core.om.model.msi.SpectrumMatch
+import fr.proline.core.om.model.msi.SpectrumProperties
 import fr.proline.core.om.model.msi.SpectrumTitleFields
-import fr.proline.core.om.provider.msi.IPTMProvider
+import fr.proline.core.om.provider.ProviderDecoratedExecutionContext
 import fr.proline.core.om.provider.msi.IPeptideProvider
-import fr.proline.core.om.provider.msi.ISeqDatabaseProvider
 import fr.proline.core.om.provider.msi.IProteinProvider
 import fr.proline.core.om.provider.msi.ProteinEmptyFakeProvider
 import fr.proline.core.om.provider.msi.ProteinFakeProvider
-import fr.proline.core.om.model.msi.SpectrumProperties
-import javax.xml.parsers.SAXParserFactory
 import javax.xml.parsers.SAXParser
-import javax.xml.parsers.ParserConfigurationException
-import org.xml.sax.SAXException
-import fr.proline.core.om.model.msi.Peptide
-import fr.proline.core.om.model.msi.PeptideMatch
-import fr.proline.core.om.model.msi.ProteinMatch
-import fr.proline.core.om.model.msi.ResultSetProperties
-import fr.proline.core.om.model.msi.XTandemImportProperties
-import scala.collection.mutable.HashMap
-import fr.proline.core.om.model.msi.Ms2Query
-import fr.profi.chemistry.model.MolecularConstants
-import fr.profi.chemistry.model.Enzyme
-import fr.proline.core.om.model.msi.LocatedPtm
-import fr.proline.core.om.model.msi.PtmDefinition
-import fr.proline.core.om.model.msi.SequenceMatch
-import fr.proline.core.om.model.msi.PeptideMatchScoreType
-import fr.proline.core.om.model.msi.Protein
-import fr.proline.core.om.model.msi.PeptideMatchProperties
-import fr.proline.core.om.model.msi.PeptideMatchXtandemProperties
+import javax.xml.parsers.SAXParserFactory
 
 class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedExecutionContext, val importProperties: Map[String, Any]) extends DefaultHandler with IResultFile with LazyLogging {
 
@@ -69,24 +57,24 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
 
   // public values
   val fileLocation: File = xtandemFile
-  var hasDecoyResultSet: Boolean = false
-  var hasMs2Peaklist: Boolean = true
   val msLevel: Int = 2
   def msQueries = msQueriesList.toArray
-  var msiSearch: MSISearch = null
-  val ionSeries = new ArrayBuffer[String]
   
   // set Providers
-  private lazy val ptmProvider = parserContext.getProvider(classOf[IPTMProvider])
   private lazy val pepProvider = parserContext.getProvider(classOf[IPeptideProvider])
-  private lazy val seqDbProvider = parserContext.getProvider(classOf[ISeqDatabaseProvider])
   private lazy val protProvider = if (parserContext.getProvider(classOf[IProteinProvider]) == null ||
     parserContext.getProvider(classOf[IProteinProvider]).isInstanceOf[ProteinEmptyFakeProvider]) {
     ProteinFakeProvider //Fake provider should at least create Fake Proteins, not None
   } else {
     parserContext.getProvider(classOf[IProteinProvider])
   }
-
+  
+  private var resultBioml: XTBioml = null
+  private lazy val xtandemSettings = new XtandemSettings(resultBioml, parserContext, instrumentConfig, peaklistSoftware)
+  lazy val msiSearch: MSISearch = xtandemSettings.msiSearchOpt.get
+//  lazy val ionSeries = xtandemSettings.ionSeries
+  lazy val hasDecoyResultSet = xtandemSettings.hasDecoyResultSet
+  lazy val hasMs2Peaklist = xtandemSettings.hasMs2Peaklist
   
   override def parseResultSet(wantDecoy: Boolean)  {
     _loadResultSet(wantDecoy) 
@@ -96,7 +84,6 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
     if (wantDecoy) {
       if (!_decoyResultSetOp.isDefined)
         _loadResultSet(true)
-
       _decoyResultSetOp.get
 
     } else {
@@ -115,7 +102,7 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
     var startTime: Long = System.currentTimeMillis()
     saxParser.parse(fileLocation, manager)
     logger.info("Xtandem file read in " + (System.currentTimeMillis() - startTime) + " milliseconds")
-    val resultBioml = manager.bioml
+    resultBioml = manager.bioml
 
     val peptideByUniqueKey = new HashMap[String, Peptide]
     val peptideMatchesByUniqueKey = new HashMap[String, ArrayBuffer[PeptideMatch]]
@@ -126,7 +113,7 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
     val parsingRules = if(this.peaklistSoftware.isDefined) this.peaklistSoftware.get.specTitleParsingRule else None
     
     // get search settings
-    val rsProps = extractMsiSearchAndProperties(resultBioml)
+    val rsProps = xtandemSettings.resultSetProperties
 
     // for each GroupModel (spectrum)
     resultBioml.groupModelList.foreach(groupItem => {
@@ -252,7 +239,7 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
               }
 
               // define the unique key for the protein
-              val seqDatabaseOpt = msiSearch.searchSettings.seqDatabases.filter(_.filePath.equals(getFastaFile(proteinItem.fileMarkup.URL).getPath)).headOption.get
+              val seqDatabaseOpt = msiSearch.searchSettings.seqDatabases.filter(_.filePath.equals(XtandemSettings.getFastaFile(proteinItem.fileMarkup.URL).getPath)).headOption.get
               val proteinAccessKey = new Tuple2(accession, seqDatabaseOpt.id)
               // get or create ProteinMatch and store it with a unique key
               if(!proteinMatchesPerUniqueKey.isDefinedAt(proteinAccessKey)) {
@@ -357,147 +344,6 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
   }
   
   def close() {}
-
-  private def extractMsiSearchAndProperties(resultBioml: XTBioml): ResultSetProperties = {
-    // parse all settings
-    val settings = new HashMap[String, String]
-    resultBioml.groupParametersList.foreach(group => {
-      group.noteList.foreach(note => {
-        settings.put(note.label, note.info)
-      })
-    })
-    
-    // get ion series requested
-    if(settings.isDefinedAt("scoring, a ions")) ionSeries += "a"
-    if(settings.isDefinedAt("scoring, b ions")) ionSeries += "b"
-    if(settings.isDefinedAt("scoring, c ions")) ionSeries += "c"
-    if(settings.isDefinedAt("scoring, x ions")) ionSeries += "x"
-    if(settings.isDefinedAt("scoring, y ions")) ionSeries += "y"
-    if(settings.isDefinedAt("scoring, z ions")) ionSeries += "z"
-    
-    // get fasta file(s)
-    val fastaFiles = new ArrayBuffer[SeqDatabase]
-    val nbProteinUsed = settings.getOrElse("modelling, total proteins used", "0").toString.toInt
-    settings.foreach { case (key, value) => {
-      if(key.startsWith("list path, sequence source #")) {
-        val fastaFile = getFastaFile(value.toString)
-        val seqDatabase = seqDbProvider.getSeqDatabase(fastaFile.getName, fastaFile.getPath)
-        if(seqDatabase.isDefined)
-          fastaFiles += seqDatabase.get
-        else
-          fastaFiles += new SeqDatabase(
-              id = SeqDatabase.generateNewId(),
-              name = fastaFile.getName,
-              filePath = fastaFile.getPath,
-              sequencesCount = nbProteinUsed, // Problem, this is the total number of proteins in all fasta files
-              searchedSequencesCount = nbProteinUsed, // Problem, this is the total number of proteins in all fasta files
-              version = "",
-              releaseDate = new java.util.Date,
-              properties = None,
-              searchProperties = None)
-      }
-    }}
-    
-    // get enzymes
-    val isSemiSpecific = settings.getOrElse("protein, cleavage semi", "no").toString.equals("yes")
-    val usedEnzymes = XTandemEnzymeVerifier.extractEnzymesFromXTandemString(parserContext, settings.getOrElse("protein, cleavage site", "").toString.toUpperCase.replaceAll("[X]", "_"), isSemiSpecific)
-    logger.debug("Enzymes: "+usedEnzymes.map(_.name).mkString(", "))
-    
-    // get PTMs
-    val refinedResults = settings.getOrElse("refine", "no").equals("yes")
-    val fixedPtmDefs = XtandemPtmVerifier.getFixedPtms(
-        ptmProvider, 
-        settings.getOrElse("protein, C-terminal residue modification mass", "").toString, 
-        settings.getOrElse("protein, N-terminal residue modification mass", "").toString, 
-        settings.getOrElse("residue, modification mass", "").toString, 
-        settings.getOrElse("refine, modification mass", "").toString, 
-        refinedResults)
-    val variablePtmDefs = XtandemPtmVerifier.getVariablePtms(
-        ptmProvider, 
-        settings.getOrElse("residue, potential modification mass", "").toString, 
-        settings.getOrElse("refine, potential modification mass", "").toString, 
-        settings.getOrElse("refine, potential C-terminus modifications", "").toString, 
-        settings.getOrElse("refine, potential N-terminus modifications", "").toString, 
-        settings.getOrElse("protein, quick acetyl", "yes").toString.equals("yes"), 
-        settings.getOrElse("protein, quick pyrolidone", "yes").toString.equals("yes"), 
-        refinedResults)
-    
-    // set SearchSettings
-    // XTandem only allows to set maximum charge state, so we have to turn 4 into "1+, 2+, 3+, 4+"
-    val ms1ChargeStates = List.range(1, settings.getOrElse("spectrum, maximum parent charge", "4").toInt + 1).map(_+"+").mkString(", ")
-    val ms1ErrorTolMinusOrPlus = settings.getOrElse("spectrum, parent monoisotopic mass error minus", settings.getOrElse("spectrum, parent monoisotopic mass error plus", 0.0))
-    val ms1ToleranceUnit = settings.getOrElse("spectrum, parent monoisotopic mass error units", "Da")
-    val ms2ToleranceUnit = settings.getOrElse("spectrum, fragment monoisotopic mass error units", "Da")
-    val searchSettings = new SearchSettings(
-        id = SearchSettings.generateNewId(),
-        softwareName = "XTandem",
-        softwareVersion = settings.getOrElse("process, version", "Unknown software version").toString,
-        taxonomy = settings.getOrElse("protein, taxon", "").toString,
-        maxMissedCleavages = settings.getOrElse("scoring, maximum missed cleavage sites", 0).toString.toInt,
-        ms1ChargeStates = ms1ChargeStates,
-        ms1ErrorTol = ms1ErrorTolMinusOrPlus.toString.toDouble,
-        ms1ErrorTolUnit = if(ms1ToleranceUnit.equals("Daltons")) "Da" else "ppm",
-        isDecoy = settings.getOrElse("scoring, include reverse", "no").equals("yes"), // Warning, it may be equal to "only" which means there's only decoy matches
-        usedEnzymes = usedEnzymes,
-        variablePtmDefs = variablePtmDefs,
-        fixedPtmDefs = fixedPtmDefs,
-        seqDatabases = fastaFiles.toArray,
-        instrumentConfig = instrumentConfig.getOrElse(null), // not available at this moment
-        msmsSearchSettings = Some(
-          new MSMSSearchSettings(
-            ms2ChargeStates = ms1ChargeStates, // not an actual setting, using MS1 value instead
-            ms2ErrorTol = settings.getOrElse("spectrum, fragment monoisotopic mass error", 0).toString.toDouble,
-            ms2ErrorTolUnit = if(ms1ToleranceUnit.equals("Daltons")) "Da" else "ppm")))
-
-    // set MSISearch
-    val inputFile = new File(settings.getOrElse("spectrum, path", "").toString) // cannot be empty !!
-    val outputFile = new File(settings.getOrElse("output, path", "output.xml").toString) // cannot be empty !!
-    val searchStartTime = if(settings.isDefinedAt("process, start time")) {
-      // date is like "2015:04:27:15:41:28"
-      val items = settings("process, start time").toString.split(":")
-      items(0)+"/"+items(1)+"/"+items(2)+" "+items(3)+":"+items(4)+":"+items(5)
-    } else ""
-    msiSearch = new MSISearch(
-      id = MSISearch.generateNewId(),
-      resultFileName = outputFile.getName,
-      searchSettings = searchSettings,
-      peakList = new Peaklist(
-          id = Peaklist.generateNewId(),
-          fileType = {
-            val extension = inputFile.getName.split('.').lastOption.getOrElse("")
-            extension.toLowerCase match {
-              case "mgf" => "Mascot generic"
-              case "dta" => "Sequest"
-              case "pkl" => "Micromass"
-              case "xml" => "mzML"
-              case _ => extension
-            }
-          },
-          path = inputFile.getPath,
-          rawFileIdentifier = inputFile.getName.split('.').headOption.getOrElse(inputFile.getName),
-          msLevel = 2,
-          peaklistSoftware = this.peaklistSoftware.getOrElse(null)),
-      date = new java.util.Date(searchStartTime),
-      title = outputFile.getName,
-      resultFileDirectory = outputFile.getParent,
-      jobNumber = 0,
-      userName = "",
-      userEmail = "",
-      queriesCount = settings.getOrElse("total spectra used", "0").toString.toInt,
-      searchedSequencesCount = nbProteinUsed)
-    
-    hasMs2Peaklist = settings.getOrElse("output, spectra", "yes").equals("yes")
-    hasDecoyResultSet = searchSettings.isDecoy
-
-    val rsProps = new ResultSetProperties()
-    val rsImportProperties = new XTandemImportProperties
-    rsImportProperties.setRawSettings(Some(settings.toMap))
-    rsProps.setXtandemImportProperties(Some(rsImportProperties))
-    
-    logger.debug("XTandem search settings gathered")
-    
-    rsProps
-  }
   
   private def getLocatedPtms(peptide: XTDomain): Array[LocatedPtm] = {
     val locatedPtms: ArrayBuffer[LocatedPtm] = new ArrayBuffer[LocatedPtm]
@@ -536,6 +382,5 @@ class XtandemParser(val xtandemFile: File, val parserContext: ProviderDecoratedE
     }
     locatedPtms.toArray
   }
-    
-  private def getFastaFile(value: String): File = new File(value.replaceAll(".pro$", "")) // xtandem may append ".pro" to a fasta file
+  
 }
