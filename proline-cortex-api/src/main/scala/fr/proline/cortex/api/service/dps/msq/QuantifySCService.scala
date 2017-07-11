c707226e6e93c9c571f196fd65481bef046e37e6
Veronique Dupierris <veronique.dupierris@cea.fr>
2017-07-11 13:37:49 +0000

    [CortexAPI] Create V02 of SpectralCount service to remove params of identi. RSM (in MasterQuantChannel) and to simply returned value

--- proline-cortex-api/src/main/scala/fr/proline/cortex/api/service/dps/msq/QuantifySCService.scala
+++ proline-cortex-api/src/main/scala/fr/proline/cortex/api/service/dps/msq/QuantifySCService.scala
@@ -12,7 +12,7 @@ import fr.profi.util.jsonrpc.JSONRPC2MethodResult
 
 object QuantifySCService extends IQuantifySCService
 
-trait IQuantifySCService extends IMsqService with IDefaultServiceVersion {
+trait IAbstractQuantifySCService extends IMsqService {
 
   /* JMS Service identification */
   val serviceLabel = "QuantifySC"
@@ -20,32 +20,11 @@ trait IQuantifySCService extends IMsqService with IDefaultServiceVersion {
     "Computes spectral count for proteins of result summaries associated to experimental design's QuantChannel." +
     "This service return the id of the created dataset (dataset_quanti_id) and the JSON result containing spectral count values (spectral_count_result)."
   )
+}
 
-  // List the handled methods
-  val methodDefinitions: Seq[IJSONRPC2Method] = List(PROCESS_METHOD)
 
-  object PROCESS_METHOD extends JSONRPC2DefaultMethod {
-
-    // Method description
-    val name = RemoteServiceIdentity.PROCESS_METHOD_NAME
-    val description = serviceDescription.get
-    
-    // Configure method interface
-    val parameters = List(
-      PROJECT_ID_PARAM,
-      NAME_PARAM,
-      DESCRIPTION_PARAM,
-      REF_RSM_ID_PARAM,
-      REF_DS_ID_PARAM,
-      PEPTIDE_REF_RSM_IDS_PARAM,
-      EXPERIMENTAL_DESIGN_PARAM
-    )
-    val returns = JSONRPC2MethodResult(
-      typeOf[Map[String, Any]],
-      "A Map containging the quant_dataset_id and the spectral_count_result as a JSON string."
-    )
-
-    object PROJECT_ID_PARAM extends JSONRPC2DefaultMethodParameter {
+trait IQuantifySCServiceParams{
+      object PROJECT_ID_PARAM extends JSONRPC2DefaultMethodParameter {
       val name = "project_id"
       val description = "The id of the project the quantitation will be created in."
       val scalaType = typeOf[Long]
@@ -79,8 +58,64 @@ trait IQuantifySCService extends IMsqService with IDefaultServiceVersion {
     object EXPERIMENTAL_DESIGN_PARAM extends JSONRPC2DefaultMethodParameter {
       val name = "experimental_design"
       val description = "The experimental design related to this quantitation."
-      val scalaType = typeOf[Object] // SimplifiedExperimentalDesign
+      val scalaType = typeOf[Object] // SimplifiedExperimentalDesign (V1) or ExperimentalDesign (V2) ?
     }
+}
+
+trait IQuantifySCService_V2 extends IAbstractQuantifySCService  {
+  
+  val serviceVersion = "2.0"
+  
+  // List the handled methods
+  val methodDefinitions: Seq[IJSONRPC2Method] = List(PROCESS_METHOD)
+
+  object PROCESS_METHOD extends JSONRPC2DefaultMethod with IQuantifySCServiceParams {
+
+    // Method description
+    val name = RemoteServiceIdentity.PROCESS_METHOD_NAME
+    val description = serviceDescription.get
+    
+    // Configure method interface
+    val parameters = List(
+      PROJECT_ID_PARAM,
+      NAME_PARAM,
+      DESCRIPTION_PARAM,
+      PEPTIDE_REF_RSM_IDS_PARAM,
+      EXPERIMENTAL_DESIGN_PARAM
+    )
+    val returns = JSONRPC2MethodResult(
+      description = "The generated quant_dataset_id.",
+      scalaType = typeOf[Long]
+    )
+  }
+}
+
+trait IQuantifySCService extends IAbstractQuantifySCService with IDefaultServiceVersion {
+
+  // List the handled methods
+  val methodDefinitions: Seq[IJSONRPC2Method] = List(PROCESS_METHOD)
+
+  object PROCESS_METHOD extends JSONRPC2DefaultMethod with IQuantifySCServiceParams {
+
+    // Method description
+    val name = RemoteServiceIdentity.PROCESS_METHOD_NAME
+    val description = serviceDescription.get
+    
+    // Configure method interface
+    val parameters = List(
+      PROJECT_ID_PARAM,
+      NAME_PARAM,
+      DESCRIPTION_PARAM,
+      REF_RSM_ID_PARAM,
+      REF_DS_ID_PARAM,
+      PEPTIDE_REF_RSM_IDS_PARAM,
+      EXPERIMENTAL_DESIGN_PARAM
+    )
+    val returns = JSONRPC2MethodResult(
+      typeOf[Map[String, Any]],
+      "A Map containging the quant_dataset_id and the spectral_count_result as a JSON string."
+    )
+
   }
 }
 
