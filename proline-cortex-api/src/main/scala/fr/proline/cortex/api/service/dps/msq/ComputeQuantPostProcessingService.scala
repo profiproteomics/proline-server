bd33962a022321d37e3cf05f8cd41a2f30eb169e
Veronique Dupierris <veronique.dupierris@cea.fr>
2019-02-01 15:34:17 +0000

    [CortexAPI] Create ComputeQuantPostProcessingService and deprecate ComputeQuantProfilesService : Same service but changed discardOxidation with discardModifcation + modifications list + qq menage coté classe de config #19279

--- proline-cortex-api/src/main/scala/fr/proline/cortex/api/service/dps/msq/ComputeQuantPostProcessingService.scala
+++ proline-cortex-api/src/main/scala/fr/proline/cortex/api/service/dps/msq/ComputeQuantPostProcessingService.scala
@@ -1,35 +1,31 @@
 package fr.proline.cortex.api.service.dps.msq
 
-import scala.reflect.runtime.universe.typeOf
-
-import fr.proline.jms.service.api.IDefaultServiceVersion
-import fr.proline.jms.service.api.RemoteServiceIdentity
 import fr.profi.util.jsonrpc.IJSONRPC2Method
 import fr.profi.util.jsonrpc.JSONRPC2DefaultMethod
 import fr.profi.util.jsonrpc.JSONRPC2DefaultMethodParameter
 import fr.profi.util.jsonrpc.JSONRPC2MethodResult
+import fr.proline.jms.service.api.IDefaultServiceVersion
+import fr.proline.jms.service.api.RemoteServiceIdentity
 
-object ComputeQuantProfilesService extends IComputeQuantProfilesService
+import scala.reflect.runtime.universe.typeOf
 
-@deprecated
-trait IComputeQuantProfilesService extends IMsqService with IDefaultServiceVersion {
+object ComputeQuantPostProcessingService extends IComputeQuantPostProcessingService
 
-  /* JMS Service identification */
-  // TODO: rename to PostProcessQuantitation
-  val serviceLabel = "ComputeQuantProfiles"
+trait IComputeQuantPostProcessingService extends  IMsqService with IDefaultServiceVersion {
+
+  val serviceLabel = "PostProcessQuantitation"
   this.serviceDescription = Some(
-    "Computes quantitative profiles of peptides and protein sets."
+    "Computes post process quantitative of peptides and protein sets."
   )
 
   // List the handled methods
   val methodDefinitions: Seq[IJSONRPC2Method] = List(PROCESS_METHOD)
-
   object PROCESS_METHOD extends JSONRPC2DefaultMethod {
 
     // Method description
     val name = RemoteServiceIdentity.PROCESS_METHOD_NAME
     val description = serviceDescription.get
-    
+
     // Configure method interface
     val parameters = List(PROJECT_ID_PARAM, MASTER_QUANT_CHANNEL_ID_PARAM, CONFIG_PARAM)
     val returns = JSONRPC2MethodResult(
@@ -44,14 +40,13 @@ trait IComputeQuantProfilesService extends IMsqService with IDefaultServiceVersi
     }
     object MASTER_QUANT_CHANNEL_ID_PARAM extends JSONRPC2DefaultMethodParameter {
       val name = "master_quant_channel_id"
-      val description = "The id of master quant channel correspoding to this quantitative dataset."
+      val description = "The id of master quant channel corresponding to this quantitative dataset."
       val scalaType = typeOf[Long]
     }
     object CONFIG_PARAM extends JSONRPC2DefaultMethodParameter {
       val name = "config"
-      val description = "Configuration used for the computation of quantitative profiles."
-      val scalaType = typeOf[Object] // ProfilizerConfig
+      val description = "Configuration used for the computation of quantitative post process."
+      val scalaType = typeOf[Map[String, Any]] // PostProcessingConfig
     }
   }
 }
-
