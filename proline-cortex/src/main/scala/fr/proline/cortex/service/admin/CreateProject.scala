a7a4b3860393cb837f2d79fc1012e78bbd640470
romdhani1987 <romdhani@live.fr>
2019-02-12 11:14:33 +0000

    [Cortex] Set project version on create a new Proline project.

--- proline-cortex/src/main/scala/fr/proline/cortex/service/admin/CreateProject.scala
+++ proline-cortex/src/main/scala/fr/proline/cortex/service/admin/CreateProject.scala
@@ -5,28 +5,29 @@ import com.typesafe.scalalogging.LazyLogging
 
 import fr.proline.admin.service.db.CreateProjectDBs
 import fr.proline.admin.service.db.SetupProline
+import fr.proline.admin.service.user.ProjectUtils
 import fr.proline.context.DatabaseConnectionContext
 import fr.proline.cortex.api.service.admin.ICreateProjectService
 import fr.proline.cortex.util.DbConnectionHelper
 import fr.proline.jms.service.api.AbstractRemoteProcessingService
 
 /**
- * JMS Service to create a new Project in current Proline DataStore. 
+ * JMS Service to create a new Project in current Proline DataStore.
  * This Service will create Project data in UDS DB and create necessary project databases (msi, lcms...)
- * 
+ *
  * Input params :
  *  name : "The project name" (String)
  *  description :  "The project description" (String)
  *  owner_id : "The project owner ID" (Long)
- *  
- * Output params : 
- *  new project Id. 
- *  
+ *
+ * Output params :
+ *  new project Id.
+ *
  */
 class CreateProject extends AbstractRemoteProcessingService with ICreateProjectService with LazyLogging {
-  
+
   //PARAM Description
-  // "name" : "The project name" : String   
+  // "name" : "The project name" : String
   // "description" :  "The project description": String
   // "owner_id" : "The project owner ID" : Long
 
@@ -38,23 +39,19 @@ class CreateProject extends AbstractRemoteProcessingService with ICreateProjectS
     val description = paramsRetriever.getOptString(PROCESS_METHOD.DESCRIPTION_PARAM, true, "")
     val name = paramsRetriever.getString(PROCESS_METHOD.NAME_PARAM)
     var prjID: Long = -1L
-    
-    val udsDbConnectionContext = new DatabaseConnectionContext(DbConnectionHelper.getDataStoreConnectorFactory().getUdsDbConnector)
+    val connectorFactory = DbConnectionHelper.getDataStoreConnectorFactory()
+    val udsDbConnectionContext = new DatabaseConnectionContext(connectorFactory.getUdsDbConnector)
 
     try {
-      
+
       // Create Data entries in UDS
-      val createPrj = new fr.proline.admin.service.user.CreateProject(udsDbConnectionContext, name, description, paramsRetriever.getLong("owner_id"))
+      val createPrj = new fr.proline.admin.service.user.CreateProject(connectorFactory, udsDbConnectionContext, name, description, paramsRetriever.getLong("owner_id"), createDbs = true)
       logger.debug("Create Project Object " + name)
       createPrj.doWork()
       prjID = createPrj.projectId
 
       if (prjID <= 0)
         throw new RuntimeException("Error creating project (" + prjID + ")")
-
-      // Create project databases
-      logger.debug("Create Project databases for project " + prjID)
-      new CreateProjectDBs(udsDbConnectionContext, SetupProline.config, prjID).doWork()
 
     } finally {
       DbConnectionHelper.tryToCloseDbContext(udsDbConnectionContext)
