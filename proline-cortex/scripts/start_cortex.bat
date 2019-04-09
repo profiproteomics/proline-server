f14bf408e3741f6613af826048a628aed3b911a0
Veronique Dupierris <veronique.dupierris@cea.fr>
2019-04-09 13:38:27 +0000

    [Cortex] change scripts : jar file is in lowercase but create a zip file using project.name parameter

--- proline-cortex/scripts/start_cortex.bat
+++ proline-cortex/scripts/start_cortex.bat
@@ -1,3 +1,3 @@
 title Proline Cortex
 rem Options: -Djava.rmi.server.hostname=132.168.72.129 -Dcom.sun.management.jmxremote.port=2194 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
-java -Xmx4G -XX:+UseG1GC -XX:+UseStringDeduplication -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30 -cp "config;Proline-Cortex-${pom.version}.jar;lib/*" -Dlogback.configurationFile=config/logback.xml fr.proline.cortex.ProcessingNode
+java -Xmx4G -XX:+UseG1GC -XX:+UseStringDeduplication -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30 -cp "config;proline-cortex-${pom.version}.jar;lib/*" -Dlogback.configurationFile=config/logback.xml fr.proline.cortex.ProcessingNode
